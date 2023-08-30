cef4555ac7752817b02900c7af76a4d8c897ae73
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-08-30 10:32:07 +0200

    git bindings

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -3,25 +3,8 @@ vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = '[G]it (fugitive)' })
 local cesc_fugitive = vim.api.nvim_create_augroup('cesc_fugitive', {})
 
 vim.keymap.set('n', '<leader>gc', ':Git checkout ', { desc = '[G]it [C]heckout' })
-local autocmd = vim.api.nvim_create_autocmd
-autocmd('BufWinEnter', {
-  group = cesc_fugitive,
-  pattern = '*',
-  callback = function()
-    if vim.bo.ft ~= 'fugitive' then
-      return
-    end
-
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.keymap.set('n', '<leader>p', ':Git push ', { desc = '[G]it [P]ush', buffer = bufnr, remap = false })
-
-    vim.keymap.set('n', '<leader>P', ':Git pull --rebase ',
-      { desc = '[G]it [P]ull rebase', buffer = bufnr, remap = false })
-
-    vim.keymap.set('n', '<leader>o', ':Git push -u origin ',
-      { desc = '[G]it Push [O]rigin', buffer = bufnr, remap = false })
-  end,
-})
+vim.keymap.set('n', '<leader>gp', ':Git push  -u origin ', { desc = '[G]it [P]ush', buffer = bufnr, remap = false })
+vim.keymap.set('n', '<leader>gP', ':Git pull --rebase ', { desc = '[G]it [P]ull rebase', buffer = bufnr, remap = false })
 
 return {
   { 'tpope/vim-fugitive' },
@@ -68,20 +51,22 @@ return {
         map('n', '<leader>gr', gs.reset_hunk, { desc = '[G]it Hunk [R]eset' })
         map('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end,
           { desc = '[G]it Hunk Reset Selection' })
+
         map('n', '<leader>gs', gs.stage_hunk, { desc = '[G]it Hunk [S]tage' })
         map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end,
           { desc = '[G]it Hunk Stage Selection' })
-
-        map('n', '<leader>hD', function() gs.diffthis '~' end, { desc = 'Hunk Diff' })
-
         map('n', '<leader>gS', gs.stage_buffer, { desc = '[G]it [S]tage Buffer' })
-        map('n', '<leader>gu', gs.undo_stage_hunk, { desc = '[G]it Hunk [U]ndo Stage' })
-        map('n', '<leader>gR', gs.reset_buffer, { desc = '[G]it [R]eset Buffer' })
-        map('n', '<leader>gp', gs.preview_hunk, { desc = '[G]it Hunk Preview' })
-        map('n', '<leader>gb', function() gs.blame_line { full = true } end, { desc = 'Hunk blame' })
-        map('n', '<leader>gd', gs.diffthis, { desc = '[G]it Hunk [D]iff' })
 
-        map('n', '<leader>vb', gs.toggle_current_line_blame, { desc = '[V]im toggle git [B]lame' })
+        map('n', '<leader>gR', gs.reset_buffer, { desc = '[G]it [R]eset Buffer' })
+
+        map('n', '<leader>gh', gs.preview_hunk, { desc = '[G]it [H]unk Preview' })
+
+        map('n', '<leader>gB', function() gs.blame_line { full = true } end, { desc = '[G]it Hunk [B]lame' })
+        map('n', '<leader>gb', gs.toggle_current_line_blame, { desc = '[G]it [B]lame line' })
+
+        map('n', '<leader>gd', gs.diffthis, { desc = '[G]it Hunk [D]iff' })
+        map('n', '<leader>gD', function() gs.diffthis '~' end, { desc = '[G]it [D]iff' })
+
         map('n', '<leader>vd', gs.toggle_deleted, { desc = '[V]im toggle git [D]eleted' })
 
         -- Text object
