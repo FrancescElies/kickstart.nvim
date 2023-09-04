88bea8b628ab05e916a758575a8951320a6558e3
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-04 10:56:07 +0200

    autocommands

--- lua/custom/plugins/autocommands.lua
+++ lua/custom/plugins/autocommands.lua
@@ -33,4 +33,50 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     end
   end,
 })
+
+function get_buffer_by_name(name, clean)
+  for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+    local buf_name = vim.fn.bufname(buffer)
+    if buf_name == name then
+      if clean then
+        vim.api.nvim_buf_set_lines(buffer, -0, -1, false, {})
+      end
+      -- buffer found
+      return buffer
+    end
+  end
+
+  -- buffer not found
+  local buffer = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_buf_set_name(buffer, name)
+  return buffer
+end
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = vim.api.nvim_create_augroup('MyTypescript', { clear = true }),
+  pattern = '*.ts',
+  callback = function()
+    local buffer = get_buffer_by_name 'typescript output'
+    local window = vim.api.nvim_get_current_win()
+    vim.api.nvim_buf_set_lines(buffer, 0, -1, false, {}) -- clear contents
+
+    vim.fn.jobstart({ 'npm', 'run', 'ci' }, {
+      stdout_buffered = true,
+      on_stdout = function(_, data)
+        if data then
+          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { 'stdout' })
+          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, data)
+        end
+      end,
+      on_stderr = function(_, data)
+        if data then
+          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { 'stderr' })
+          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, data)
+          vim.api.nvim_win_set_buf(window, buffer) -- make buffer visible
+        end
+      end,
+    })
+  end,
+})
+
 return {}
