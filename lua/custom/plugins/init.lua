f1100ac1600667799211fd7cd5dd90dec5ff5add
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-04 06:00:05 +0200

    harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -75,20 +75,18 @@ return {
   {
     'ThePrimeagen/harpoon',
     config = function()
-      require('harpoon').setup()
-      vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file, { desc = '[H]arpoon [A]dd file' })
-      vim.keymap.set('n', '<leader>ht', require('harpoon.ui').toggle_quick_menu,
-        { desc = '[H]arpoon [T]oggle quick menu' })
-      vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next, { desc = '[H]arpoon nav [N]ext' })
-      vim.keymap.set('n', '<leader>hp', require('harpoon.ui').nav_prev, { desc = '[H]arpoon nav [P]revious' })
-      vim.keymap.set('n', '<leader>h1', function() require('harpoon.ui').nav_file(1) end,
-        { desc = '[H]arpoon goto file [1]' })
-      vim.keymap.set('n', '<leader>h2', function() require('harpoon.ui').nav_file(2) end,
-        { desc = '[H]arpoon goto file [2]' })
-      vim.keymap.set('n', '<leader>h3', function() require('harpoon.ui').nav_file(3) end,
-        { desc = '[H]arpoon goto file [3]' })
-      vim.keymap.set('n', '<leader>h4', function() require('harpoon.ui').nav_file(4) end,
-        { desc = '[H]arpoon goto file [4]' })
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = '[H]arpoon [A]dd file' })
+      vim.keymap.set('n', '<leader>ht', ui.toggle_quick_menu, { desc = '[H]arpoon [T]oggle quick menu' })
+      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+      vim.keymap.set('n', '<leader>hn', ui.nav_next, { desc = '[H]arpoon nav [N]ext' })
+      vim.keymap.set('n', '<leader>hp', ui.nav_prev, { desc = '[H]arpoon nav [P]revious' })
+      vim.keymap.set('n', '<leader>h1', function() ui.nav_file(1) end, { desc = '[H]arpoon goto file [1]' })
+      vim.keymap.set('n', '<leader>h2', function() ui.nav_file(2) end, { desc = '[H]arpoon goto file [2]' })
+      vim.keymap.set('n', '<leader>h3', function() ui.nav_file(3) end, { desc = '[H]arpoon goto file [3]' })
+      vim.keymap.set('n', '<leader>h4', function() ui.nav_file(4) end, { desc = '[H]arpoon goto file [4]' })
     end,
     dependencies = {
       'nvim-lua/plenary.nvim',
