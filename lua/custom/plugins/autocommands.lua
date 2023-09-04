c00a2dd9aad1707ff7aae5a7aafaaf4a504a4cdd
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-04 15:04:36 +0200

    autocommand

--- lua/custom/plugins/autocommands.lua
+++ lua/custom/plugins/autocommands.lua
@@ -64,43 +64,44 @@ function string_not_empty(str)
   end
 end
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-  group = vim.api.nvim_create_augroup('MyTypescript', { clear = true }),
-  pattern = '*.ts',
-  callback = function()
-    local buffer = get_buffer_by_name_or_scratch 'typescript output'
-    local window = vim.api.nvim_get_current_win()
-    vim.api.nvim_buf_set_lines(buffer, 0, -1, false, {}) -- clear contents
+function check_for_typescript_errors(buffer, data)
+  for _, line in ipairs(data) do
+    if string.find(line, ': error ') ~= nil then
+      vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { '❌' })
+      return
+    end
+  end
+  vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { '✅' })
+end
 
-    vim.fn.jobstart({ 'npm', 'run', 'build' }, {
-      stdout_buffered = true,
-      on_stdout = function(_, data)
+local function attach_to_buffer(command)
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    group = vim.api.nvim_create_augroup('MyAutoRun', { clear = true }),
+    callback = function()
+      local buffer = get_buffer_by_name_or_scratch 'my-autorun'
+      local window = vim.api.nvim_get_current_win()
+      -- clear contents
+      vim.api.nvim_buf_set_lines(buffer, 0, -1, false, {})
+      local function append(_, data)
         if data then
-          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { 'stdout' })
           vim.api.nvim_buf_set_lines(buffer, -1, -1, false, data)
+          check_for_typescript_errors(buffer, data)
+        end
+        vim.api.nvim_win_set_buf(window, buffer) -- make buffer visible
+      end
 
-          -- check for typescript errors
-          for _, line in ipairs(data) do
-            if string.find(line, ': error ') ~= nil then
-              vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { '❌' })
-              vim.api.nvim_win_set_buf(window, buffer) -- make buffer visible
-              return
-            end
-          end
-          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { '✅' })
-          vim.api.nvim_win_set_buf(window, buffer) -- make buffer visible
-        end
-      end,
-      on_stderr = function(_, data)
-        if data and #data >= 1 and string_not_empty(data[1]) then
-          print(data[1])
-          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { 'stderr' })
-          vim.api.nvim_buf_set_lines(buffer, -1, -1, false, data)
-          vim.api.nvim_win_set_buf(window, buffer) -- make buffer visible
-        end
-      end,
-    })
-  end,
-})
+      vim.fn.jobstart({ 'npm', 'run', 'build' }, {
+        stdout_buffered = true,
+        on_stdout = append,
+        on_stderr = append,
+      })
+    end,
+  })
+end
+
+vim.api.nvim_create_user_command('MyAutoRun', function()
+  local command = vim.split(vim.fn.input 'Command:', ' ')
+  attach_to_buffer(command)
+end, {})
 
 return {}
