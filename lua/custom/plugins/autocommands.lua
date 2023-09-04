be5673c29bfaa74fffd4ef1e3294256391a0165f
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-04 05:40:57 +0200

    autocommands

---
+++ lua/custom/plugins/autocommands.lua
@@ -0,0 +1,35 @@
+vim.api.nvim_create_augroup('bufcheck', { clear = true })
+
+-- reload config file on change
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = 'bufcheck',
+  pattern = vim.env.MYVIMRC,
+  command = 'silent source %',
+})
+
+-- start terminal in insert mode
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = 'bufcheck',
+  pattern = '*',
+  command = 'startinsert | set winfixheight',
+})
+
+-- start git messages in insert mode
+vim.api.nvim_create_autocmd('FileType', {
+  group = 'bufcheck',
+  pattern = { 'gitcommit', 'gitrebase' },
+  command = 'startinsert | 1',
+})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = 'bufcheck',
+  pattern = '*',
+  callback = function()
+    if vim.fn.line '\'"' > 0 and vim.fn.line '\'"' <= vim.fn.line '$' then
+      vim.fn.setpos('.', vim.fn.getpos '\'"')
+      -- vim.cmd('normal zz') -- how do I center the buffer in a sane way??
+      vim.cmd 'silent! foldopen'
+    end
+  end,
+})
