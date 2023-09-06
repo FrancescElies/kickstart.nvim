b31e5d0f84842985ca29159300d2b7a2db980158
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-06 07:23:21 +0200

    use alt as window control modifier

--- lua/custom/plugins/wezterm.lua
+++ lua/custom/plugins/wezterm.lua
@@ -2,10 +2,10 @@ return {
   'FrancescElies/wezterm-mux.nvim',
   config = function()
     local mux = require 'wezterm-mux'
-    vim.keymap.set('n', '<C-h>', mux.wezterm_move_left)
-    vim.keymap.set('n', '<C-l>', mux.wezterm_move_right)
-    vim.keymap.set('n', '<C-j>', mux.wezterm_move_down)
-    vim.keymap.set('n', '<C-k>', mux.wezterm_move_up)
+    vim.keymap.set('n', '<A-h>', mux.wezterm_move_left)
+    vim.keymap.set('n', '<A-l>', mux.wezterm_move_right)
+    vim.keymap.set('n', '<A-j>', mux.wezterm_move_down)
+    vim.keymap.set('n', '<A-k>', mux.wezterm_move_up)
     vim.keymap.set('n', '<A-x>', '<C-w>q')
   end,
 }
