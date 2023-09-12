078deaf74a72f65b624d7c9de1394107795fbb72
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-12 16:02:31 +0200

    autocommand .js file open original .ts file

--- lua/custom/plugins/autocommands.lua
+++ lua/custom/plugins/autocommands.lua
@@ -33,16 +33,35 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     end
   end,
 })
---
--- Return to last edit position when opening files
+
+-- When opening a javascript file opens it's associated
+-- typescript file if a mapping is found
+-- Searches for something like
+--     //# sourceMappingURL=my.file.js.map
 vim.api.nvim_create_autocmd('BufReadPost', {
   group = 'bufcheck',
-  pattern = '*',
+  pattern = '*.js',
   callback = function()
-    if vim.fn.line '\'"' > 0 and vim.fn.line '\'"' <= vim.fn.line '$' then
-      vim.fn.setpos('.', vim.fn.getpos '\'"')
-      -- vim.cmd('normal zz') -- how do I center the buffer in a sane way??
-      vim.cmd 'silent! foldopen'
+    local buffnr = vim.api.nvim_get_current_buf()
+    local bufname = vim.fn.bufname(buffnr)
+    local lines = vim.api.nvim_buf_get_lines(buffnr, 0, -1, false)
+    for _, line in pairs(lines) do
+      local map_file = string.match(line, '//# sourceMappingURL=(.+)')
+      if map_file ~= nil then
+        local bufpath = vim.fn.getcwd()
+        local mapping_file_path = vim.fn.resolve(bufpath .. "/" .. map_file)
+        local mapping_file = io.open(mapping_file_path)
+        if mapping_file == nil then
+          return
+        end
+        print("js file mapping found: " .. mapping_file_path)
+        local mapping_file_contents = mapping_file:read("*a")
+        mapping_file:close()
+        local ts_file = string.match(mapping_file_contents, '%["(.-%.ts)"%]')
+        vim.cmd('e ' .. ts_file)
+        vim.cmd(':set filetype=typescript')
+        return
+      end
     end
   end,
 })
@@ -93,7 +112,7 @@ local function attach_to_buffer(pattern, command)
   })
 end
 
-attach_to_buffer('*.ts', { 'npm', 'run', 'build' })
+attach_to_buffer('*.ts', { 'npm', 'run', 'build_dev' })
 
 vim.api.nvim_create_user_command('AutoRun', function()
   local command = vim.split(vim.fn.input 'Command:', ' ')
