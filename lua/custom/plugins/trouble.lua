5bdbd6a01c5e12a764e7eac0e7902ea947b330e0
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-09-13 14:32:00 +0200

    trouble

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -9,9 +9,10 @@ return {
   },
   {
     'folke/trouble.nvim',
+    config = true,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
-      { '<leader>xx', function() require('trouble').open() end, desc = 'quickfi[x] trouble all' },
+      { '<leader>xx', function() require('trouble').open() end,               desc = 'quickfi[x] trouble all' },
       {
         '<leader>xw',
         function() require('trouble').open 'workspace_diagnostics' end,
@@ -27,9 +28,9 @@ return {
         function() require('trouble').open 'document_diagnostics' end,
         desc = 'quickfi[x] [d]ocument diagnostics',
       },
-      { '<leader>xq', function() require('trouble').open 'quickfix' end, desc = '[q]uickfi[x]' },
-      { '<leader>xl', function() require('trouble').open 'loclist' end, desc = 'quickfi[x] loclist' },
-      { 'gR', function() require('trouble').open 'lsp_references' end },
+      { '<leader>xq', function() require('trouble').open 'quickfix' end,      desc = '[q]uickfi[x]' },
+      { '<leader>xl', function() require('trouble').open 'loclist' end,       desc = 'quickfi[x] loclist' },
+      { 'gR',         function() require('trouble').open 'lsp_references' end },
     },
   },
 }
