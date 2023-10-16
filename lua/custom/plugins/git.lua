f07e0702534852c0e9f7f4d07f17b729f3d30869
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-10-16 14:03:26 +0200

    remove lazygit add git worktree

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -140,10 +140,24 @@ return {
   },
   { 'sindrets/diffview.nvim' },
   {
-    'kdheepak/lazygit.nvim',
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-lua/plenary.nvim',
+    'ThePrimeagen/git-worktree.nvim',
+    callback = function()
+      require("telescope").load_extension("git_worktree")
+    end,
+    keys = {
+      {
+        '<leader>gws',
+        ":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
+        desc = '[G]it [W]orkspace [S]earch'
+      },
+      -- <Enter> - switches to that worktree
+      -- <c-d> - deletes that worktree
+      -- <c-f> - toggles forcing of the next deletion
+      {
+        '<leader>gwc',
+        ":lua require('telescope').extensions.git_worktree.create_git_worktree()",
+        desc = '[G]it [W]orkspace [C]reate'
+      }
     },
   },
 }
