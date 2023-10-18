c3a6213cc59015d8d307da7e2dcd534f1fed1646
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-10-18 15:01:48 +0200

    bring back none-ls

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,29 +1,29 @@
 -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
 
 return {
-  -- 'nvimtools/none-ls.nvim',
-  -- event = { 'BufReadPre', 'BufNewFile' },
-  -- dependencies = { 'mason.nvim' },
-  -- opts = function()
-  --   local nls = require 'null-ls'
-  --   return {
-  --     root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
-  --     sources = {
-  --       nls.builtins.formatting.fish_indent,
-  --       nls.builtins.diagnostics.fish,
-  --       nls.builtins.formatting.stylua,
-  --       nls.builtins.formatting.biome,
-  --       -- nls.builtins.formatting.spell,
-  --       nls.builtins.formatting.shfmt,
-  --       nls.builtins.formatting.buf,
-  --       nls.builtins.formatting.black,
-  --       nls.builtins.formatting.ruff,
-  --       nls.builtins.diagnostics.ruff,
-  --       nls.builtins.diagnostics.ltrs,
-  --       nls.builtins.formatting.rustfmt,
-  --       nls.builtins.diagnostics.typos,
-  --       -- nls.builtins.diagnostics.flake8,
-  --     },
-  --   }
-  -- end,
+  'nvimtools/none-ls.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = { 'mason.nvim' },
+  opts = function()
+    local nls = require 'null-ls'
+    return {
+      root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
+      sources = {
+        nls.builtins.formatting.fish_indent,
+        nls.builtins.diagnostics.fish,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.biome,
+        -- nls.builtins.formatting.spell,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.formatting.buf,
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.ruff,
+        nls.builtins.diagnostics.ruff,
+        nls.builtins.diagnostics.ltrs,
+        nls.builtins.formatting.rustfmt,
+        nls.builtins.diagnostics.typos,
+        -- nls.builtins.diagnostics.flake8,
+      },
+    }
+  end,
 }
