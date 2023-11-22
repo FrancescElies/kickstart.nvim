5ad168a4543463d224f115ff321ba8d68fa5ab76
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-11-22 13:51:15 +0100

    remove unused plugin

--- init.lua
+++ init.lua
@@ -85,7 +85,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -109,7 +109,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',   opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
@@ -139,7 +139,7 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',  opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
@@ -405,14 +405,10 @@ require('nvim-treesitter.configs').setup {
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '[w', function() vim.diagnostic.goto_prev { severity = 'WARN' } end,
-  { desc = 'Go to previous warning message' })
-vim.keymap.set('n', ']w', function() vim.diagnostic.goto_next { severity = 'WARN' } end,
-  { desc = 'Go to next warning message' })
-vim.keymap.set('n', '[e', function() vim.diagnostic.goto_prev { severity = 'ERROR' } end,
-  { desc = 'Go to previous error message' })
-vim.keymap.set('n', ']e', function() vim.diagnostic.goto_next { severity = 'ERROR' } end,
-  { desc = 'Go to next error message' })
+vim.keymap.set('n', '[w', function() vim.diagnostic.goto_prev { severity = 'WARN' } end, { desc = 'Go to previous warning message' })
+vim.keymap.set('n', ']w', function() vim.diagnostic.goto_next { severity = 'WARN' } end, { desc = 'Go to next warning message' })
+vim.keymap.set('n', '[e', function() vim.diagnostic.goto_prev { severity = 'ERROR' } end, { desc = 'Go to previous error message' })
+vim.keymap.set('n', ']e', function() vim.diagnostic.goto_next { severity = 'ERROR' } end, { desc = 'Go to next error message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>E', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
@@ -451,12 +447,10 @@ local on_attach = function(_, bufnr)
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-    '[W]orkspace [L]ist Folders')
+  nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_) vim.lsp.buf.format() end,
-    { desc = 'Format current buffer with LSP' })
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_) vim.lsp.buf.format() end, { desc = 'Format current buffer with LSP' })
 end
 
 -- Enable the following language servers
@@ -478,9 +472,12 @@ clangd_capabilities.offsetEncoding = { 'utf-16' }
 local lspconfig = require 'lspconfig'
 lspconfig.nushell.setup {
   cmd = { 'nu', '--lsp' },
-  filetypes = { 'nu' },
   root_dir = require('lspconfig.util').find_git_ancestor,
   single_file_support = true,
+  -- capabilities = capabilities,
+  on_attach = on_attach,
+  -- settings = servers[server_name],
+  filetypes = { 'nu' },
 }
 
 local servers = {
