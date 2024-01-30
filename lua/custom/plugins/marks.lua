fa28bb4bbe45b531a06860cda1e3120096fbd06d
cesc <elies@posteo.net>
2024-01-30 11:37:18 +0100

    marks

--- lua/custom/plugins/marks.lua
+++ lua/custom/plugins/marks.lua
@@ -1,3 +1,7 @@
+vim.keymap.set('n', '<leader>vm', require("telescope.builtin").marks, { desc = '[v]im [m]arks' })
+-- shadows register `l`, that's ok
+vim.keymap.set('n', 'ml', require("telescope.builtin").marks, { desc = '[m]arks list' })
+
 return {
     'chentoast/marks.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim', 'renerocksai/calendar-vim' },
