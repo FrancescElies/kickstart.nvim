c9bd075c7ee218211e09776885f58780c73bc9c8
Francesc Elies <francesc.elies@mbbm-ast.com>
2024-02-02 12:45:38 +0100

    trouble

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,36 +1,67 @@
 return {
   {
-    'folke/todo-comments.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = true,
     keys = {
-      { '<leader>xt', ':TodoQuickFix', desc = 'quickfi[x] [t]odo' },
+      { "<leader>xt", ":TodoQuickFix", desc = "quickfi[x] [t]odo" },
     },
   },
   {
-    'folke/trouble.nvim',
+    "folke/trouble.nvim",
     config = true,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-      { '<leader>xx', function() require('trouble').open() end,               desc = 'quickfi[x] trouble all' },
       {
-        '<leader>xw',
-        function() require('trouble').open 'workspace_diagnostics' end,
-        desc = 'quickfi[x] [w]orkspace diagnostics',
+        "<leader>xx",
+        function() require("trouble").open() end,
+        desc = "quickfi[x] trouble all",
       },
       {
-        '<leader>dx',
-        function() require('trouble').open 'document_diagnostics' end,
-        desc = '[d]ocument diagnostics quickfi[x] ',
+        "<leader>xw",
+        function() require("trouble").open("workspace_diagnostics") end,
+        desc = "quickfi[x] [w]orkspace diagnostics",
       },
       {
-        '<leader>xd',
-        function() require('trouble').open 'document_diagnostics' end,
-        desc = 'quickfi[x] [d]ocument diagnostics',
+        "<leader>dx",
+        function() require("trouble").open("document_diagnostics") end,
+        desc = "[d]ocument diagnostics quickfi[x] ",
       },
-      { '<leader>xq', function() require('trouble').open 'quickfix' end,      desc = '[q]uickfi[x]' },
-      { '<leader>xl', function() require('trouble').open 'loclist' end,       desc = 'quickfi[x] loclist' },
-      { 'gR',         function() require('trouble').open 'lsp_references' end },
+      {
+        "<leader>xd",
+        function() require("trouble").open("document_diagnostics") end,
+        desc = "quickfi[x] [d]ocument diagnostics",
+      },
+      {
+        "<leader>xq",
+        function() require("trouble").open("quickfix") end,
+        desc = "[q]uickfi[x]",
+      },
+      {
+        "<leader>xl",
+        function() require("trouble").open("loclist") end,
+        desc = "quickfi[x] loclist",
+      },
+      {
+        "[x",
+        function() require("trouble").previous({ skip_groups = true, jump = true }) end,
+        desc = "[x]trouble previous",
+      },
+      {
+        "]x",
+        function() require("trouble").next({ skip_groups = true, jump = true }) end,
+        desc = "[x]trouble next",
+      },
+      {
+        "[X",
+        function() require("trouble").first({ skip_groups = true, jump = true }) end,
+        desc = "[x]trouble firstprevious",
+      },
+      {
+        "]X",
+        function() require("trouble").last({ skip_groups = true, jump = true }) end,
+        desc = "[x]trouble last",
+      },
     },
   },
 }
