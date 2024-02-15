463530aa20a8018095f86275c7940544860020a2
Francesc Elies <francesc.elies@mbbm-ast.com>
2024-02-15 14:04:57 +0100

    keymap

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -38,10 +38,10 @@ vim.keymap.set('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev buffer' })
 vim.keymap.set('n', ']b', '<cmd>bnext<cr>', { desc = 'Next buffer' })
 
 -- quickfix
-vim.keymap.set('n', '[q', '<cmd>cprevious<cr>zz', { desc = '[Q]uickfix previous' })
-vim.keymap.set('n', ']q', '<cmd>cnext<cr>zz', { desc = '[Q]uickfix next' })
-vim.keymap.set('n', '[Q', '<cmd>cfirst<cr>zz', { desc = '[Q]uickfix [F]irst]' })
-vim.keymap.set('n', ']Q', '<cmd>clast<cr>zz', { desc = '[Q]uickfix [L]ast' })
+vim.keymap.set('n', '[q', '<cmd>cprevious<cr>zz', { desc = 'Quickfix previous' })
+vim.keymap.set('n', ']q', '<cmd>cnext<cr>zz', { desc = 'Quickfix next' })
+vim.keymap.set('n', '[Q', '<cmd>cfirst<cr>zz', { desc = 'Quickfix First' })
+vim.keymap.set('n', ']Q', '<cmd>clast<cr>zz', { desc = 'Quickfix Last' })
 
 -- Keep things vertically centered during searches
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -50,23 +50,25 @@ vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- Move lines in visual mode
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', '<leader>/', ':s/\\\\/\\//g<cr>', { desc = '[R]eplace \\ -> [/]' })
-vim.keymap.set('v', '<leader>\\', ':s/\\//\\\\/g<cr>', { desc = '[R]eplace / -> [\\]' })
-vim.keymap.set('v', '<leader>l', ':luafile %<cr>', { desc = '[L]oad luafile' })
+-- vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+-- vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<leader>/', ':s/\\\\/\\//g<cr>', { desc = 'Replace \\ -> /' })
+vim.keymap.set('v', '<leader>\\', ':s/\\//\\\\/g<cr>', { desc = 'Replace / -> [\\]' })
 
 -- Reload configuration
-vim.keymap.set('n', '<leader>vf', ':FormatToggle<CR>', { desc = '[V]im toggle [F]ormat' })
-vim.keymap.set('n', '<leader>vn', toggle_number, { desc = '[V]im toggle line [N]umber' })
-vim.keymap.set('n', '<leader>vs', ':set invspell<cr>', { desc = '[V]im toggle [S]pell' })
-vim.keymap.set('n', '<leader>vh', ':set invhlsearch<cr>', { desc = '[H]ighlight search' })
+vim.keymap.set('n', '<leader>vl', ':luafile %<cr>', { desc = 'Vim load Luafile' })
+vim.keymap.set('n', '<leader>ve', ':edit $MYVIMRC<cr>', { desc = 'Vim edit config' })
+vim.keymap.set('n', '<leader>vr', ':source $MYVIMRC<cr>', { desc = 'Vim reload config' })
+vim.keymap.set('n', '<leader>vf', ':FormatToggle<CR>', { desc = 'Vim toggle Format' })
+vim.keymap.set('n', '<leader>vn', toggle_number, { desc = 'Vim toggle line Number' })
+vim.keymap.set('n', '<leader>vs', ':set invspell<cr>', { desc = 'Vim toggle Spell' })
+vim.keymap.set('n', '<leader>vh', ':set invhlsearch<cr>', { desc = 'Highlight search' })
 
 -- tabs
 vim.keymap.set('n', '<leader>tl', '<cmd>tablast<cr>', { desc = 'Last Tab' })
 vim.keymap.set('n', '<leader>tf', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
 vim.keymap.set('n', '<leader>tn', '<cmd>tabnew<cr>', { desc = 'New Tab' })
-vim.keymap.set('n', '<leader>td', '<cmd>tabclose<cr>', { desc = '[D]elete/Close Tab' })
+vim.keymap.set('n', '<leader>td', '<cmd>tabclose<cr>', { desc = 'Delete/Close Tab' })
 vim.keymap.set('n', ']t', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
 vim.keymap.set('n', '[t', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
 
