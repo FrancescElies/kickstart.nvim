707942b4e00f9df5ac5ccb3bf5781854a0217f69
Francesc Elies <francesc.elies@mbbm-ast.com>
2024-02-15 11:17:52 +0100

    remove nvim nu

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -63,12 +63,12 @@ return {
       local plugin = require('lazy.core.config').spec.plugins['mini.surround']
       local opts = require('lazy.core.plugin').values(plugin, 'opts', false)
       local mappings = {
-        { opts.mappings.add,            desc = 'Add surrounding',                     mode = { 'n', 'v' } },
-        { opts.mappings.delete,         desc = 'Delete surrounding' },
-        { opts.mappings.find,           desc = 'Find right surrounding' },
-        { opts.mappings.find_left,      desc = 'Find left surrounding' },
-        { opts.mappings.highlight,      desc = 'Highlight surrounding' },
-        { opts.mappings.replace,        desc = 'Replace surrounding' },
+        { opts.mappings.add, desc = 'Add surrounding', mode = { 'n', 'v' } },
+        { opts.mappings.delete, desc = 'Delete surrounding' },
+        { opts.mappings.find, desc = 'Find right surrounding' },
+        { opts.mappings.find_left, desc = 'Find left surrounding' },
+        { opts.mappings.highlight, desc = 'Highlight surrounding' },
+        { opts.mappings.replace, desc = 'Replace surrounding' },
         { opts.mappings.update_n_lines, desc = 'Update `MiniSurround.config.n_lines`' },
       }
       mappings = vim.tbl_filter(function(m)
@@ -78,12 +78,12 @@ return {
     end,
     opts = {
       mappings = {
-        add = 'gza',            -- Add surrounding in Normal and Visual modes
-        delete = 'gzd',         -- Delete surrounding
-        find = 'gzf',           -- Find surrounding (to the right)
-        find_left = 'gzF',      -- Find surrounding (to the left)
-        highlight = 'gzh',      -- Highlight surrounding
-        replace = 'gzr',        -- Replace surrounding
+        add = 'gza', -- Add surrounding in Normal and Visual modes
+        delete = 'gzd', -- Delete surrounding
+        find = 'gzf', -- Find surrounding (to the right)
+        find_left = 'gzF', -- Find surrounding (to the left)
+        highlight = 'gzh', -- Highlight surrounding
+        replace = 'gzr', -- Replace surrounding
         update_n_lines = 'gzn', -- Update `n_lines`
       },
     },
@@ -100,15 +100,6 @@ return {
       local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
       local cmp = require 'cmp'
       cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
-  },
-  {
-    'LhKipp/nvim-nu',
-    config = function()
-      require('nu').setup()
-    end,
-    callback = function()
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = true })
     end,
   },
 }
