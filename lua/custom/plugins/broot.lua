fa80f5d49897283121d12265f093739b0fc68726
Francesc Elies <francesc.elies@mbbm-ast.com>
2024-02-16 13:34:01 +0100

    Quickly see whales

--- lua/custom/plugins/broot.lua
+++ lua/custom/plugins/broot.lua
@@ -7,5 +7,6 @@ return {
   config = function()
     vim.keymap.set('n', '<C-p>', ':BrRoot<cr>', { desc = 'broot' })
     vim.keymap.set('n', '<leader>fb', ':Br<cr>', { desc = 'broot (current dir)' })
+    vim.keymap.set('n', '<leader>fw', ':Br -w<cr>', { desc = 'find whales (current dir)' })
   end,
 }
