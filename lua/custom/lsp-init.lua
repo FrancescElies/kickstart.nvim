87e034ee600c6e8cd9f0878850789db7b3f7cdc2
Francesc Elies <elies@posteo.net>
2024-04-29 07:35:14 +0200

    reenable lspconfig

--- lua/custom/lsp-init.lua
+++ lua/custom/lsp-init.lua
@@ -61,66 +61,69 @@ M.servers = {
   --   capabilities = M.jsonls_capabilities,
   -- },
   ruff_lsp = {},
+
   -- slint_lsp = { filetypes = { 'slint' }, capabilities = slint_capabilities },
-  -- rust_analyzer = {
-  --   ['rust-analyzer'] = {
-  --     diagnostics = { enable = true },
-  --     check = { command = 'check' }, -- can be clippy too
-  --     cargo = { features = 'all' },
-  --     checkOnSave = {
-  --       assist = {
-  --         importGranularity = 'module',
-  --         importPrefix = 'by_self',
-  --       },
-  --       cargo = { loadOutDirsFromCheck = true },
-  --       procMacro = { enable = true },
-  --       command = 'clippy',
-  --       inlayHints = true,
-  --     },
-  --     inlayHints = {
-  --       enabled = true,
-  --       chainingHints = { enable = false }, -- do not enable
-  --       closingBraceHints = { enable = false },
-  --       bindingModeHints = { enable = true },
-  --       closureCaptureHints = { enable = true },
-  --       closureReturnTypeHints = { enable = 'always' },
-  --       expressionAdjustmentHints = { enable = 'always' },
-  --       lifetimeElisionHints = { enable = 'always', useParameterNames = true },
-  --       reborrowHints = { enable = 'always' },
-  --       typeHints = { hideClosureInitialization = true, hideNamedConstructor = true },
-  --       locationLinks = false,
-  --     },
-  --     completion = {
-  --       completionItem = {
-  --         commitCharactersSupport = true,
-  --         deprecatedSupport = true,
-  --         documentationFormat = { 'markdown', 'plaintext' },
-  --         preselectSupport = true,
-  --         snippetSupport = true,
-  --       },
-  --     },
-  --     signatureHelp = {
-  --       dynamicRegistration = true,
-  --       signatureInformation = {
-  --         activeParameterSupport = true,
-  --         documentationFormat = { 'markdown', 'plaintext' },
-  --         parameterInformation = {
-  --           labelOffsetSupport = true,
-  --         },
-  --       },
-  --     },
-  --     procMacro = {
-  --       enable = true,
-  --       methodReference = true,
-  --     },
-  --     lens = {
-  --       enable = true,
-  --       methodReferences = true,
-  --       references = true,
-  --       implementations = false,
-  --     },
-  --   },
-  -- },
+
+  rust_analyzer = {
+    ['rust-analyzer'] = {
+      diagnostics = { enable = true },
+      check = { command = 'check' }, -- can be clippy too
+      cargo = { features = 'all' },
+      checkOnSave = {
+        assist = {
+          importGranularity = 'module',
+          importPrefix = 'by_self',
+        },
+        cargo = { loadOutDirsFromCheck = true },
+        procMacro = { enable = true },
+        command = 'clippy',
+        inlayHints = true,
+      },
+      inlayHints = {
+        enabled = true,
+        chainingHints = { enable = false }, -- do not enable
+        closingBraceHints = { enable = false },
+        bindingModeHints = { enable = true },
+        closureCaptureHints = { enable = true },
+        closureReturnTypeHints = { enable = 'always' },
+        expressionAdjustmentHints = { enable = 'always' },
+        lifetimeElisionHints = { enable = 'always', useParameterNames = true },
+        reborrowHints = { enable = 'always' },
+        typeHints = { hideClosureInitialization = true, hideNamedConstructor = true },
+        locationLinks = false,
+      },
+      completion = {
+        completionItem = {
+          commitCharactersSupport = true,
+          deprecatedSupport = true,
+          documentationFormat = { 'markdown', 'plaintext' },
+          preselectSupport = true,
+          snippetSupport = true,
+        },
+      },
+      signatureHelp = {
+        dynamicRegistration = true,
+        signatureInformation = {
+          activeParameterSupport = true,
+          documentationFormat = { 'markdown', 'plaintext' },
+          parameterInformation = {
+            labelOffsetSupport = true,
+          },
+        },
+      },
+      procMacro = {
+        enable = true,
+        methodReference = true,
+      },
+      lens = {
+        enable = true,
+        methodReferences = true,
+        references = true,
+        implementations = false,
+      },
+    },
+  },
+
   tsserver = {
     init_options = {
       provideFormatter = false,
--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,5 +1,6 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^4', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  --   disable rust-analyzer in lsp-init.lua if this plugin enabled
+  --   'mrcjkb/rustaceanvim',
+  --   version = '^4', -- Recommended
+  --   lazy = false, -- This plugin is already lazy
 }
