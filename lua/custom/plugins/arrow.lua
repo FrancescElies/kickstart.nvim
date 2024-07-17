6f7b0887fe29f4a7e0dd7293eb591e9ff2e0dbb3
Francesc Elies <elies@posteo.net>
2024-07-17 12:20:08 +0200

    marks

--- lua/custom/plugins/arrow.lua
+++ lua/custom/plugins/arrow.lua
@@ -1,8 +1,9 @@
 return {
-  'otavioschwanck/arrow.nvim',
-  opts = {
-    show_icons = true,
-    leader_key = ';', -- Recommended to be a single key
-    buffer_leader_key = 'm', -- Per Buffer Mappings
-  },
+  -- Probably telescope marks is enough
+  -- 'otavioschwanck/arrow.nvim',
+  -- opts = {
+  --   show_icons = true,
+  --   leader_key = ';', -- Recommended to be a single key
+  --   buffer_leader_key = 'm', -- Per Buffer Mappings
+  -- },
 }
