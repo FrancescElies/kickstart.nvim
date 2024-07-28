f5045c2f3f9eda91b4fd156e1f35b6d35e1c9bd3
Francesc Elies <elies@posteo.net>
2024-07-28 20:58:22 +0300

    pane move stop at edge

--- lua/custom/plugins/annotations.lua
+++ lua/custom/plugins/annotations.lua
@@ -1,22 +1,22 @@
 local opts = { noremap = true, silent = true }
 
--- require('which-key').register {
---   ['<leader>a'] = { name = '[a]nnotate', _ = 'which_key_ignore' },
--- }
+require('which-key').add {
+  ['<leader>a'] = { name = '[a]nnotate', _ = 'which_key_ignore' },
+}
 
 return {
-  -- 'danymat/neogen',
-  -- dependencies = 'nvim-treesitter/nvim-treesitter',
-  -- config = true,
-  -- -- Uncomment next line if you want to follow only stable versions
-  -- version = '*',
-  -- keys = {
-  --   { '<leader>Af', ":lua require('neogen').generate({ type = 'func' })<CR>", desc = '[a]nnotate function', opts },
-  --   {
-  --     '<leader>Ac',
-  --     ":lua require('neogen').generate({ type = 'class' })<CR>",
-  --     desc = '[a]nnotate [c]lass',
-  --     opts,
-  --   },
-  -- },
+  'danymat/neogen',
+  dependencies = 'nvim-treesitter/nvim-treesitter',
+  config = true,
+  -- Uncomment next line if you want to follow only stable versions
+  version = '*',
+  keys = {
+    { '<leader>Af', ":lua require('neogen').generate({ type = 'func' })<CR>", desc = '[a]nnotate function', opts },
+    {
+      '<leader>Ac',
+      ":lua require('neogen').generate({ type = 'class' })<CR>",
+      desc = '[a]nnotate [c]lass',
+      opts,
+    },
+  },
 }
--- lua/custom/plugins/wezterm.lua
+++ lua/custom/plugins/wezterm.lua
@@ -1,5 +1,10 @@
 return {
   'mrjones2014/smart-splits.nvim',
+  config = function()
+    require('smart-splits').setup {
+      at_edge = 'stop',
+    }
+  end,
   keys = {
     {
       '<A-h>',
