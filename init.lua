781e01bda82e2b0c496136eaa415a8852551fc32
Francesc Elies <elies@posteo.net>
2024-07-29 06:00:15 +0200

    add te[x]t section

--- init.lua
+++ init.lua
@@ -282,6 +282,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>x', group = 'te[x]t' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
     end,
--- lua/custom/plugins/annotations.lua
+++ lua/custom/plugins/annotations.lua
@@ -1,9 +1,5 @@
 local opts = { noremap = true, silent = true }
 
-require('which-key').add {
-  { '<leader>a', group = '[a]annotate' },
-}
-
 return {
   'danymat/neogen',
   dependencies = 'nvim-treesitter/nvim-treesitter',
@@ -11,12 +7,6 @@ return {
   -- Uncomment next line if you want to follow only stable versions
   version = '*',
   keys = {
-    { '<leader>Af', ":lua require('neogen').generate({ type = 'func' })<CR>", desc = '[a]nnotate function', opts },
-    {
-      '<leader>Ac',
-      ":lua require('neogen').generate({ type = 'class' })<CR>",
-      desc = '[a]nnotate [c]lass',
-      opts,
-    },
+    { '<leader>xa', ':Neogen func', desc = '[a]nnotate class, func, file, type', opts },
   },
 }
