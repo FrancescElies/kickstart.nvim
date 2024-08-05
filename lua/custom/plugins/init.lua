1cbb5119c764a46c083b2ce9bad35592aa659e0b
Francesc Elies <elies@posteo.net>
2024-08-05 10:36:30 +0200

    redir

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,6 +11,17 @@ function p(v)
   print(vim.inspect(v))
 end
 
+-- https://www.reddit.com/r/neovim/comments/zhweuc/whats_a_fast_way_to_load_the_output_of_a_command/
+-- Example:
+-- :Redir =vim.lsp.get_active_clients()
+vim.api.nvim_create_user_command('Redir', function(ctx)
+  local result = vim.api.nvim_exec2(ctx.args, { output = true })
+  local lines = vim.split(result.output, '\n', { plain = true })
+  vim.cmd 'new'
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = '+', complete = 'command' })
+
 return {
   -- automatically follow symlinks
   { 'aymericbeaumet/vim-symlink', dependencies = { 'moll/vim-bbye' } },
