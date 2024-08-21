e02e1267da942e93b7f4b8457d57f8839e55b601
Francesc Elies <elies@posteo.net>
2024-08-21 08:17:21 +0200

    oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,6 +22,19 @@ vim.api.nvim_create_user_command('Redir', function(ctx)
   vim.opt_local.modified = false
 end, { nargs = '+', complete = 'command' })
 
+local function get_git_root()
+  local dot_git_path = vim.fn.finddir('.git', '.;')
+  return vim.fn.fnamemodify(dot_git_path, ':h')
+end
+
+vim.api.nvim_create_user_command('CdCurrentBufferDir', function()
+  vim.api.nvim_set_current_dir(vim.fn.expand '%:p:h')
+end, {})
+
+vim.api.nvim_create_user_command('CdGitRoot', function()
+  vim.api.nvim_set_current_dir(get_git_root())
+end, {})
+
 return {
   -- automatically follow symlinks
   { 'aymericbeaumet/vim-symlink', dependencies = { 'moll/vim-bbye' } },
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,5 +1,11 @@
+local opts = { noremap = true, silent = true }
+
 return {
   'stevearc/oil.nvim',
   opts = {},
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '-', ':Oil<cr>', desc = 'open parent dir', opts },
+    { '<leader>fd', ':Oil<cr>', desc = 'open parent [d]dir', opts },
+  },
 }
