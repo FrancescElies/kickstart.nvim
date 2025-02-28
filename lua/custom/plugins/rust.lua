aef61024159beb74902563510eb91a15e45e0a90
Francesc Elies <elies@posteo.net>
2025-02-28 16:12:48 +0100

    .

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,3 +1,44 @@
+-- local bufnr = vim.api.nvim_get_current_buf()
+-- https://github.com/Canop/codesort
+-- cargo install codesort
+--https://github.com/dsully/nvim/blob/ddcd5971bc061440d3b0e798ffa15edfd6a7f77b/after/ftplugin/rust.lua#L1
+-- if vim.fn.executable 'codesort' == 1 then
+--   --
+--   keys.bmap('<leader>cs', function()
+--     --
+--     local result = vim
+--       .system({
+--         'codesort',
+--         '--around',
+--         tostring(vim.api.nvim_win_get_cursor(0)[1]),
+--         '--detect',
+--         vim.fs.normalize(vim.fs.basename(vim.api.nvim_buf_get_name(0))),
+--       }, {
+--         stdin = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false),
+--       })
+--       :wait()
+--
+--     if result.code == 0 and result.stdout then
+--       vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(result.stdout, '\n', { trimempty = true }))
+--     else
+--       vim.notify('Error running codesort: ' .. (result.stderr or ''), vim.log.levels.ERROR)
+--     end
+--   end, 'Sort code', bufnr)
+--
+--   keys.xmap('<leader>cs', function()
+--     local start_line = vim.api.nvim_buf_get_mark(0, '<')[1]
+--     local end_line = vim.api.nvim_buf_get_mark(0, '>')[1]
+--
+--     local result = vim.system({ 'codesort' }, { stdin = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false) }):wait()
+--
+--     if result.code == 0 and result.stdout then
+--       vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, vim.split(result.stdout, '\n', { trimempty = true }))
+--     else
+--       vim.notify('Error running codesort: ' .. (result.stderr or ''), vim.log.levels.ERROR)
+--     end
+--   end, 'Sort code', bufnr)
+-- end
+
 return {
   --   disable rust-analyzer in lsp-init.lua if this plugin enabled
   'mrcjkb/rustaceanvim',
