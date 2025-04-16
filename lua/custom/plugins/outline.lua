a4f2459aa087c6d4383007f0d2dde58120f13e7d
Francesc Elies <elies@posteo.net>
2025-04-16 08:52:34 +0200

    .

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -1,24 +1,24 @@
--- return {
---   'hedyhli/outline.nvim',
---   lazy = true,
---   cmd = { 'Outline', 'OutlineOpen' },
---   keys = { -- Example mapping to toggle outline
---     { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
---   },
---   opts = {
---     -- Your setup opts here
---   },
--- }
-
 return {
-  'stevearc/aerial.nvim',
-  opts = {},
-  -- Optional dependencies
+  'hedyhli/outline.nvim',
+  lazy = true,
+  cmd = { 'Outline', 'OutlineOpen' },
   keys = {
-    { '<leader>o', '<cmd>AerialToggle!<CR>', desc = 'Toggle outline' },
+    { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
   },
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
+  opts = {
+    -- Your setup opts here
   },
 }
+
+-- return {
+--   'stevearc/aerial.nvim',
+--   opts = {},
+--   -- Optional dependencies
+--   keys = {
+--     { '<leader>o', '<cmd>AerialToggle!<CR>', desc = 'Toggle outline' },
+--   },
+--   dependencies = {
+--     'nvim-treesitter/nvim-treesitter',
+--     'nvim-tree/nvim-web-devicons',
+--   },
+-- }
