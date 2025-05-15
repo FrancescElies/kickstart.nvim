a138146c8141065c3370949672bb3655f95a4c7f
Francesc Elies <elies@posteo.net>
2025-05-15 07:25:40 +0200

    zen size

--- lua/custom/plugins/zenmode.lua
+++ lua/custom/plugins/zenmode.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<leader>z', function()
   require('zen-mode').setup {
-    window = { width = 100, options = {} },
+    window = { width = 120, options = {} },
   }
   require('zen-mode').toggle()
   vim.wo.wrap = false
