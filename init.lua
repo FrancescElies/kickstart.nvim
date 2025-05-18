dd62ed554e3446b7363e04b13ae398f22b69eb92
Francesc Elies <elies@posteo.net>
2025-05-18 13:30:40 +0200

    js formatter

--- init.lua
+++ init.lua
@@ -794,7 +794,7 @@ require('lazy').setup({
         end,
         typescript = { 'biome' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
         --
         -- Use the "*" filetype to run formatters on all filetypes.
         ['*'] = { 'typos' },
