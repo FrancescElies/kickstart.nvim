27cbac80d05a472e648ae54e41e67239bd4e9e28
Francesc Elies <elies@posteo.net>
2025-05-27 17:50:37 +0200

    workspace diag

--- lua/custom/plugins/workspace_diagnostic.lua
+++ lua/custom/plugins/workspace_diagnostic.lua
@@ -1,14 +1,14 @@
-vim.api.nvim_set_keymap('n', '<leader>x', '', {
-  noremap = true,
-  desc = 'worksapce diagnostics',
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    for _, client in ipairs(vim.lsp.get_clients { bufnr = bufnr }) do
-      require('workspace-diagnostics').populate_workspace_diagnostics(client, 0)
-    end
-  end,
-})
+-- vim.api.nvim_set_keymap('n', '<leader>x', '', {
+--   noremap = true,
+--   desc = 'worksapce diagnostics',
+--   callback = function()
+--     local bufnr = vim.api.nvim_get_current_buf()
+--     for _, client in ipairs(vim.lsp.get_clients { bufnr = bufnr }) do
+--       require('workspace-diagnostics').populate_workspace_diagnostics(client, 0)
+--     end
+--   end,
+-- })
 
 return {
-  'artemave/workspace-diagnostics.nvim',
+  -- 'artemave/workspace-diagnostics.nvim',
 }
