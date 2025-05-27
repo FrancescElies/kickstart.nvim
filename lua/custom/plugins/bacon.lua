a7495338a1c5efc8a63202a26265bcc4767c7240
Francesc Elies <elies@posteo.net>
2025-05-27 11:02:57 +0200

    bacon disable

--- lua/custom/plugins/bacon.lua
+++ lua/custom/plugins/bacon.lua
@@ -1,16 +1,16 @@
 return {
-  'Canop/nvim-bacon',
-  config = function()
-    vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-      pattern = { '*.rs', '*.toml' },
-      group = vim.api.nvim_create_augroup('my-rust-bacon', { clear = true }),
-      callback = function(event)
-        local nkeymap = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'bacon: ' .. desc })
-        end
-        nkeymap('!', ':BaconLoad<CR>:w<CR>:BaconNext<CR>', 'next bacon issue')
-        nkeymap(',', ':BaconList<CR>', 'bacon list')
-      end,
-    })
-  end,
+  -- 'Canop/nvim-bacon',
+  -- config = function()
+  --   vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  --     pattern = { '*.rs', '*.toml' },
+  --     group = vim.api.nvim_create_augroup('my-rust-bacon', { clear = true }),
+  --     callback = function(event)
+  --       local nkeymap = function(keys, func, desc)
+  --         vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'bacon: ' .. desc })
+  --       end
+  --       nkeymap('!', ':BaconLoad<CR>:w<CR>:BaconNext<CR>', 'next bacon issue')
+  --       nkeymap(',', ':BaconList<CR>', 'bacon list')
+  --     end,
+  --   })
+  -- end,
 }
