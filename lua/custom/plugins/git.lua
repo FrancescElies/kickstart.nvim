340e4e29a97ff217efbab8fc2a58589e981f3f78
Francesc Elies <elies@posteo.net>
2025-06-15 15:07:54 +0200

    git

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -9,6 +9,7 @@ vim.keymap.set('n', 'gl', '<cmd>diffget //3<cr>', { desc = 'get right diff' })
 vim.keymap.set('n', '<leader>gb', telebin.git_branches, { desc = '[G]it [B]ranches' })
 vim.keymap.set('n', '<leader>gL', telebin.git_bcommits, { desc = '[G]it [L]og this [B]uffer' })
 vim.keymap.set('n', '<leader>gl', telebin.git_commits, { desc = '[G]it [L]og' })
+vim.keymap.set('v', '<leader>gl', telebin.git_bcommits_range, { desc = '[G]it [L]og' }) --- Lists commits for a range of lines in the current buffer with diff preview
 vim.keymap.set('n', '<leader>gf', telebin.git_files, { desc = '[G]it [F]iles' })
 vim.keymap.set('n', '<leader>g.', telebin.git_status, { desc = '[G]it [S]tatus' })
 vim.keymap.set('n', '<leader>gz', telebin.git_stash, { desc = '[G]it zstash' })
@@ -55,6 +56,7 @@ return {
       { '<leader>gp', '<cmd>Git! push --force-with-lease -u origin <cr>', desc = '[G]it [P]ush force with lease' },
       { '<leader>gP', '<cmd>Git! pull --rebase <cr>', desc = '[G]it [P]ull rebase' },
       { '<leader>gc', '<cmd>Git commit<cr>', desc = '[G]it [c]ommit' },
+      { '<leader>gu', '<cmd>Git reset HEAD~1<cr>', desc = '[G]it [u]ndo last commit' },
     },
   },
   {
