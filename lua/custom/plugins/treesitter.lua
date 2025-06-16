0498a406aa1d0deda1ac2c84b6b20d887dc0f9b4
Francesc Elies <elies@posteo.net>
2025-06-16 13:21:23 +0200

    fix remove conflict

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -110,12 +110,13 @@ return {
             -- Below will go to either the start or the end, whichever is closer.
             -- Use if you want more granular movements
             -- Make it even more gradual by adding multiple queries and regex.
-            goto_next = {
-              [']d'] = '@conditional.outer',
-            },
-            goto_previous = {
-              ['[d'] = '@conditional.outer',
-            },
+            -- TODO: conflicts with next diagnostic ]d
+            -- goto_next = {
+            --   [']d'] = '@conditional.outer',
+            -- },
+            -- goto_previous = {
+            --   ['[d'] = '@conditional.outer',
+            -- },
           },
         },
       }
