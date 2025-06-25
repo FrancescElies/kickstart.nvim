37ec3bb9baf958d03a8c5c232d35547e69a1104c
Francesc Elies <elies@posteo.net>
2025-06-25 11:37:44 +0200

    zenmode

--- lua/custom/plugins/zenmode.lua
+++ lua/custom/plugins/zenmode.lua
@@ -1,15 +1,15 @@
+local function zen_mode()
+  require('zen-mode').setup { window = { width = 120, options = {} } }
+  require('zen-mode').toggle()
+  vim.wo.wrap = true
+  vim.wo.number = true
+  vim.wo.rnu = true
+end
+
 return {
   'folke/zen-mode.nvim',
   keys = {
-    {
-      '<leader>vz',
-      function()
-        require('zen-mode').setup { window = { width = 120, options = {} } }
-        require('zen-mode').toggle()
-        vim.wo.wrap = true
-        vim.wo.number = true
-        vim.wo.rnu = true
-      end,
-    },
+    { '<leader>vz', zen_mode },
+    { '<M-z>', zen_mode },
   },
 }
