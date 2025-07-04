735056cab4458b2caf1a9f09862bfa8c9080c28e
Francesc Elies <elies@posteo.net>
2025-07-04 09:17:25 +0200

    no context

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     config = function()
       require('treesitter-context').setup {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
       }
     end,
   },
