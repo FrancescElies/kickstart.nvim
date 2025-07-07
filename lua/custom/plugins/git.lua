ccd1034d58cfb9e763d23d01ee72cd3c31548686
Francesc Elies <elies@posteo.net>
2025-07-07 22:06:22 +0200

    git

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -8,14 +8,15 @@ local tele = require 'telescope.builtin'
 vim.keymap.set('n', 'gh', '<cmd>diffget //2<cr>', { desc = 'get left diff' })
 vim.keymap.set('n', 'gl', '<cmd>diffget //3<cr>', { desc = 'get right diff' })
 
-vim.keymap.set('n', '<leader>gB', tele.git_branches, { desc = '[G]it [B]ranches' })
-vim.keymap.set('n', '<leader>gL', tele.git_bcommits, { desc = '[G]it [L]og this [B]uffer' })
-vim.keymap.set('n', '<leader>gl', tele.git_commits, { desc = '[G]it [L]og' })
-vim.keymap.set('v', '<leader>gl', tele.git_bcommits_range, { desc = '[G]it [L]og' }) --- Lists commits for a range of lines in the current buffer with diff preview
+vim.keymap.set('n', '<leader>gB', tele.git_branches, { desc = '[g]it [b]ranches' })
+vim.keymap.set('n', '<leader>gL', tele.git_bcommits, { desc = '[g]it [l]og this [b]uffer' })
+vim.keymap.set('n', '<leader>gl', tele.git_commits, { desc = '[g]it [l]og' })
+--- Lists commits for a range of lines in the current buffer with diff preview
+-- vim.keymap.set('v', '<leader>gl', tele.git_bcommits_range, { desc = '[g]it [l]og' })
 -- stylua: ignore
-vim.keymap.set('n', '<leader>gf', function() tele.git_files { use_file_path = true } end, { desc = '[G]it [F]iles' })
-vim.keymap.set('n', '<leader>g.', tele.git_status, { desc = '[G]o [G]it' })
-vim.keymap.set('n', '<leader>gz', tele.git_stash, { desc = '[G]it zstash' })
+vim.keymap.set('n', '<leader>gf', function() tele.git_files { use_file_path = true } end, { desc = '[g]it [f]iles' })
+vim.keymap.set('n', '<leader>g.', tele.git_status, { desc = '[g]o [g]it' })
+vim.keymap.set('n', '<leader>gz', tele.git_stash, { desc = '[g]it zstash' })
 
 --
 return {
@@ -26,7 +27,7 @@ return {
       'sindrets/diffview.nvim', -- optional - Diff integration
       'nvim-telescope/telescope.nvim', -- optional
     },
-    keys = { { '<leader>gs', vim.cmd.Neogit, desc = '[G]it [S]status' } },
+    keys = { { '<leader>gs', vim.cmd.Neogit, desc = '[g]it [s]status' } },
   },
   -- {
   --   'tpope/vim-fugitive',
@@ -53,28 +54,28 @@ return {
   --     { '<leader>gr', '<cmd>Gread<cr>', desc = 'Git restore current file' },
   --     { '<leader>gs', vim.cmd.Git, desc = '[g]it [s]tatus (fugitive)' },
   --     { '<leader>gf', '<cmd>Git fetch<cr>', desc = 'Git fetch' },
-  --     { '<leader>gp', '<cmd>Git! push --force-with-lease -u origin <cr>', desc = '[G]it [P]ush force with lease' },
-  --     { '<leader>gP', '<cmd>Git! pull --rebase <cr>', desc = '[G]it [P]ull rebase' },
-  --     { '<leader>gc', '<cmd>Git commit<cr>', desc = '[G]it [c]ommit' },
-  --     { '<leader>gu', '<cmd>Git reset HEAD~1<cr>', desc = '[G]it [u]ndo last commit' },
+  --     { '<leader>gp', '<cmd>Git! push --force-with-lease -u origin <cr>', desc = '[g]it [p]ush force with lease' },
+  --     { '<leader>gP', '<cmd>Git! pull --rebase <cr>', desc = '[g]it [p]ull rebase' },
+  --     { '<leader>gc', '<cmd>Git commit<cr>', desc = '[g]it [c]ommit' },
+  --     { '<leader>gu', '<cmd>Git reset HEAD~1<cr>', desc = '[g]it [u]ndo last commit' },
   --   },
   -- },
   {
     'rhysd/git-messenger.vim',
-    keys = { { '<leader>gm', ':GitMessenger<CR>', desc = '[G]it [M]essenger' } },
+    keys = { { '<leader>gm', ':GitMessenger<CR>', desc = '[g]it [m]essenger' } },
   },
   {
     'sindrets/diffview.nvim',
     opts = {},
     keys = {
-      { '<leader>gdf', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it [D]iff [F]ile history (this file)' },
-      { '<leader>gdl', ':DiffviewFileHistory', desc = '[G]it [D]iff History ([l]ine evolution)', mode = { 'n', 'v' } },
-      { '<leader>gdb', ':DiffviewFileHistory', desc = '[G]it [D]iff History (this [b]ranch)' },
+      -- :h diff-mode
+      -- :h copy-diffs
+      { '<leader>gdb', ':DiffviewFileHistory<cr>', desc = '[g]it [d]iff History current [b]ranch' },
+      { '<leader>gdf', '<cmd>DiffviewFileHistory %', desc = '[g]it [d]iff [f]ile current [f]ile' },
+      { '<leader>gdw', '<cmd>DiffviewOpen<cr>', desc = '[g]it [d]iff working tree' },
+      { '<leader>gdb', '<cmd>DiffviewOpen origin/main...HEAD', desc = '[g]it [d]iff against merge [b]ase' },
+      { '<leader>gdl', ':DiffviewFileHistory', desc = '[g]it [d]iff History ([l]ine evolution)', mode = { 'v' } },
 
-      { '<leader>gdo', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff [O]pen (merge conflicts)' },
-      { '<leader>gdB', '<cmd>DiffviewOpen origin/main...HEAD', desc = '[G]it [D]iff merge [B]ase' },
-      { '<leader>gdc', '<cmd>DiffviewClose<cr>', desc = '[G]it [D]iff [C]lose' },
-      -- { '<leader>gdl', ':AdvancedGitSearch diff_commit_line<cr>', mode = { 'n', 'v' }, desc = '[G]it [D]iff [L]ine' },
       -- Examples
       -- :DiffviewOpen
       -- :DiffviewOpen HEAD~2
@@ -100,9 +101,9 @@ return {
       require('telescope').load_extension 'advanced_git_search'
     end,
     keys = {
-      { '<leader>g/', ':AdvancedGitSearch<CR>', desc = '[G]it [S]earch' },
-      { '<leader>gdl', ':AdvancedGitSearch diff_commit_line<cr>', mode = { 'n', 'v' }, desc = '[G]it [D]iff [L]ine' },
-      { '<leader>gR', ':AdvancedGitSearch checkout_reflog<cr>', desc = '[G]it [R]eflog' },
+      { '<leader>g/', ':AdvancedGitSearch<CR>', desc = '[g]it [s]earch' },
+      { '<leader>gdL', ':AdvancedGitSearch diff_commit_line<cr>', mode = { 'n', 'v' }, desc = '[g]it [d]iff [l]ine' },
+      { '<leader>gR', ':AdvancedGitSearch checkout_reflog<cr>', desc = '[g]it [r]eflog' },
     },
     dependencies = {
       'nvim-telescope/telescope.nvim',
