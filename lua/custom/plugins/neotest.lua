4f44ef92c8f4a309f1044ccca6b502549ecdff0b
Francesc Elies <elies@posteo.net>
2025-07-07 21:04:26 +0200

    neotest

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,38 @@
+return {
+  -- An extensible framework for interacting with tests within NeoVim
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-plenary',
+      'rouge8/neotest-rust',
+    },
+    keys = {
+      {
+        '<leader>n',
+        function()
+          require('neotest').run.run()
+        end,
+      },
+      {
+        '<leader>N',
+        function()
+          require('neotest').output.open { enter = true }
+        end,
+      },
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-plenary',
+          require 'neotest-rust' {
+            args = { '--no-capture' },
+          },
+        },
+      }
+    end,
+  },
+}
