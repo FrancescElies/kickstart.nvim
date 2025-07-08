d67d6ef67ca126613afb81d01006103796769626
Francesc Elies <elies@posteo.net>
2025-07-08 17:47:36 +0200

    neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -11,20 +11,15 @@ return {
       'rouge8/neotest-rust',
     },
     keys = {
-      {
-        '<leader>n',
-        function()
-          require('neotest').run.run()
-        end,
-      },
-      {
-        '<leader>N',
-        function()
-          require('neotest').output.open { enter = true }
-        end,
-      },
+      -- stylua: ignore start
+      { '<leader>rn', function() require('neotest').run.run() end, { desc = "run nearest test" } },
+      { '<leader>rb', function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "run tests current buf" } },
+      { '<leader>ro', function() require('neotest').output.open { enter = true } end, { desc = "open result" }},
+      { '<leader>rd', function() require("neotest").run.run {vim.fn.expand("%"), suite = false, strategy = "dap"} end, { desc = "run tests current buf" } },
+      -- stylua: ignore end
     },
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require('neotest').setup {
         adapters = {
           require 'neotest-plenary',
