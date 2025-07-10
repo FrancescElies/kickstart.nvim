e1d5e842098e984aad7467de8c9418cc4121a5c5
Francesc Elies <elies@posteo.net>
2025-07-10 18:20:03 +0200

    filebrowser

--- lua/custom/plugins/file-browser.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-telescope/telescope-file-browser.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-  keys = {
-    { '-', ':Telescope file_browser<CR>' },
-    { '_', ':Telescope file_browser path=%:p:h select_buffer=true<CR>' },
-  },
-}
