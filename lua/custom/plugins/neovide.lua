d7a5f8a36a02303cc8115f9837014e4f38cd2686
Francesc Elies <elies@posteo.net>
2025-07-12 10:52:52 +0200

    neovide

---
+++ lua/custom/plugins/neovide.lua
@@ -0,0 +1,35 @@
+--
+-- Neovide
+--
+if vim.g.neovide then
+  local copy_key = '<C-S-C>'
+  local paste_key = '<C-S-V>'
+
+  vim.keymap.set('v', copy_key, '"+y') -- Copy
+  vim.keymap.set('n', paste_key, '"+P') -- Paste normal mode
+  vim.keymap.set('v', paste_key, '"+P') -- Paste visual mode
+  vim.keymap.set('c', paste_key, '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', paste_key, '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set('n', '<M-=>', function()
+    change_scale_factor(1.10)
+  end)
+  vim.keymap.set('n', '<M-->', function()
+    change_scale_factor(1 / 1.10)
+  end)
+  vim.keymap.set('n', '<M-0>', function()
+    vim.g.neovide_scale_factor = 1.0
+  end)
+
+  -- Allow clipboard copy paste in neovim
+  vim.api.nvim_set_keymap('', paste_key, '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', paste_key, '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', paste_key, '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', paste_key, '<C-R>+', { noremap = true, silent = true })
+end
+
+return {}
