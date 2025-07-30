93ce8d90f68ec6fc852eaa06f0b2a5d6489e7bb2
Francesc elies <elies@posteo.net>
2025-07-30 14:28:08 +0200

    mason install stuff manually

--- init.lua
+++ init.lua
@@ -763,7 +763,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         -- 'clangd',
-        'codespell',
+        -- 'codespell',
         -- 'harper_ls',
         -- 'markdownlint',
         'typos',
@@ -774,7 +774,7 @@ require('lazy').setup({
         -- 'powershell_es',
         -- 'ruff', -- python linter and formatter
         -- 'ruff-lsp', -- python linter and formatter
-        'sqlfluff',
+        -- 'sqlfluff',
         'stylua', -- Used to format lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
