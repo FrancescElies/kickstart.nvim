1b7dc77f031715dee71023b6947927f756a8c992
Francesc Elies <elies@posteo.net>
2025-08-02 08:42:54 +0200

    not using arrow

--- lua/custom/plugins/arrow.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'otavioschwanck/arrow.nvim',
-  opts = {
-    show_icons = true,
-    leader_key = 'M',
-    -- leader_key = 'M-m>',
-    buffer_leader_key = 'm', -- Per Buffer Mappings
-  },
-}
