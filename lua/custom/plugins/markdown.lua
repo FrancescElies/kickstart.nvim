da5e523ece5b8f589eb1713612f7b9e7574ae3fb
Francesc Elies <elies@posteo.net>
2025-09-05 09:15:03 +0200

    markdown

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,14 +1,7 @@
--- For `plugins/markview.lua` users.
 return {
-  'OXY2DEV/markview.nvim',
-  lazy = false,
-
-  -- For `nvim-treesitter` users.
-  priority = 49,
-
-  -- For blink.cmp's completion
-  -- source
-  -- dependencies = {
-  --     "saghen/blink.cmp"
-  -- },
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
 }
