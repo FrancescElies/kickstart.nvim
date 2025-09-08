d60e7eb9321e68fd95ffdb406fd1ad7e31a5d1ef
Francesc Elies <elies@posteo.net>
2025-09-08 09:29:57 +0200

    bring back windows moves

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -38,7 +38,7 @@ end
 local function is_quickfix_open()
   return vim.fn.getqflist({ winid = 0 }).winid ~= 0
 end
-vim.keymap.set('n', '<C-k>', function()
+vim.keymap.set('n', '<C-S-k>', function()
   if is_quickfix_open() then
     vim.cmd 'cprevious' -- previous quickfix item
     vim.cmd 'normal! zz'
@@ -47,7 +47,7 @@ vim.keymap.set('n', '<C-k>', function()
     jump_diagnostic_by_severity { count = -1 }
   end
 end)
-vim.keymap.set('n', '<C-j>', function()
+vim.keymap.set('n', '<C-S-j>', function()
   if is_quickfix_open() then
     vim.cmd 'cnext' -- next quickfix item
     vim.cmd 'normal! zz'
