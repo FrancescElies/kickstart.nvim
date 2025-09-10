ca8f011e49574ede1bcbfc35647b554a2f231198
Francesc Elies <elies@posteo.net>
2025-09-10 11:18:07 +0200

    lsp

--- init.lua
+++ init.lua
@@ -754,8 +754,183 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local my_lsp_cfg = require 'custom.lsp-init'
-      local servers = my_lsp_cfg.servers
+      local servers = {
+        clangd = {
+          cmd = { 'clangd', '--log=verbose', '--enable-config' },
+        },
+        -- gopls = {},
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- tsserver = {},
+        --
+        -- powershell_es = {
+        --   settings = {
+        --     powershell = {
+        --       -- https://github.com/PowerShell/PowerShellEditorServices/blob/main/docs/guide/getting_started.md
+        --       -- https://github.com/PowerShell/PowerShellEditorServices/blob/main/src/PowerShellEditorServices/Services/Workspace/LanguageServerSettings.cs
+        --       codeFormatting = { Preset = 'OTBS' },
+        --     },
+        --   },
+        -- },
+        lua_ls = {
+          -- cmd = {...},
+          -- filetypes { ...},
+          -- capabilities = {},
+          settings = {
+            Lua = {
+              workspace = { checkThirdParty = false },
+              telemetry = { enable = false },
+              completion = {
+                callSnippet = 'Replace',
+              },
+              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              diagnostics = { disable = { 'missing-fields' } },
+            },
+          },
+        },
+        -- biome = { filetypes = { 'typescript', 'json', 'maxpat', 'json' }, init_options = { provideFormatter = true } },
+        -- sqls = {},
+        -- gopls = {},
+        -- basedpyright = {}, -- pyright fork with inlay hints
+        -- pyright = {},
+        -- jsonls = {
+        --   filetypes = { 'maxpat', 'json' },
+        --   init_options = { provideFormatter = true },
+        --   capabilities = M.jsonls_capabilities,
+        -- },
+        --   capabilities = M.jsonls_capabilities,
+        -- },
+        -- ruff = {},
+
+        -- slint_lsp = { filetypes = { 'slint' }, capabilities = slint_capabilities },
+
+        -- using rustaceanvim instead
+        -- rust_analyzer = {
+        --   ['rust-analyzer'] = {
+        --     diagnostics = { enable = true },
+        --     check = { command = 'check' }, -- can be clippy too
+        --     cargo = { features = 'all' },
+        --     checkOnSave = {
+        --       assist = {
+        --         importGranularity = 'module',
+        --         importPrefix = 'by_self',
+        --       },
+        --       cargo = { loadOutDirsFromCheck = true },
+        --       procMacro = { enable = true },
+        --       command = 'clippy',
+        --       inlayHints = true,
+        --     },
+        --     inlayHints = {
+        --       enabled = true,
+        --       chainingHints = { enable = false }, -- do not enable
+        --       closingBraceHints = { enable = false },
+        --       bindingModeHints = { enable = true },
+        --       closureCaptureHints = { enable = true },
+        --       closureReturnTypeHints = { enable = 'always' },
+        --       expressionAdjustmentHints = { enable = 'always' },
+        --       lifetimeElisionHints = { enable = 'always', useParameterNames = true },
+        --       reborrowHints = { enable = 'always' },
+        --       typeHints = { hideClosureInitialization = true, hideNamedConstructor = true },
+        --       locationLinks = false,
+        --     },
+        --     completion = {
+        --       completionItem = {
+        --         commitCharactersSupport = true,
+        --         deprecatedSupport = true,
+        --         documentationFormat = { 'markdown', 'plaintext' },
+        --         preselectSupport = true,
+        --         snippetSupport = true,
+        --       },
+        --     },
+        --     signatureHelp = {
+        --       dynamicRegistration = true,
+        --       signatureInformation = {
+        --         activeParameterSupport = true,
+        --         documentationFormat = { 'markdown', 'plaintext' },
+        --         parameterInformation = {
+        --           labelOffsetSupport = true,
+        --         },
+        --       },
+        --     },
+        --     procMacro = {
+        --       enable = true,
+        --       methodReference = true,
+        --     },
+        --     lens = {
+        --       enable = true,
+        --       methodReferences = true,
+        --       references = true,
+        --       implementations = false,
+        --     },
+        --   },
+        -- },
+
+        -- ts_ls = {
+        --   init_options = {
+        --     provideFormatter = false,
+        --   },
+        --   typescript = {
+        --     inlayHints = {
+        --       includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all'
+        --       includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        --       includeInlayVariableTypeHints = true,
+        --       includeInlayFunctionParameterTypeHints = true,
+        --       includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        --       includeInlayPropertyDeclarationTypeHints = true,
+        --       includeInlayFunctionLikeReturnTypeHints = true,
+        --       includeInlayEnumMemberValueHints = true,
+        --     },
+        --   },
+        --   javascript = {
+        --     inlayHints = {
+        --       includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all'
+        --       includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        --       includeInlayVariableTypeHints = true,
+        --
+        --       includeInlayFunctionParameterTypeHints = true,
+        --       includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        --       includeInlayPropertyDeclarationTypeHints = true,
+        --       includeInlayFunctionLikeReturnTypeHints = true,
+        --       includeInlayEnumMemberValueHints = true,
+        --     },
+        --   },
+        -- },
+        -- html = { filetypes = { 'html', 'twig', 'hbs' } },
+        -- omnisharp = {
+        --   -- cmd = { 'dotnet', '~/bin/omnisharp/Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.dll' },
+        --   enable_editorconfig_support = true,
+        --   enable_ms_build_load_projects_on_demand = false,
+        --   enable_roslyn_analyzers = true,
+        --   organize_imports_on_format = true,
+        --   enable_import_completion = false,
+        --   sdk_include_prereleases = true,
+        --   analyze_open_documents_only = false,
+        -- },
+
+        -- zls = {
+        --   -- https://zigtools.org/zls/editors/vim/nvim-lspconfig/
+        --   -- Whether to enable build-on-save diagnostics
+        --   --
+        --   -- Further information about build-on save:
+        --   -- https://zigtools.org/zls/guides/build-on-save/
+        --   enable_build_on_save = true,
+        --
+        --   -- Neovim already provides basic syntax highlighting
+        --   semantic_tokens = 'partial',
+        --
+        --   -- omit the following line if `zig` is in your PATH
+        --   zig_exe_path = vim.fn.exepath 'zig',
+        --   enable_snippets = true,
+        --   enable_autofix = true,
+        --   enable_ast_check_diagnostics = true,
+        -- },
+      }
 
       -- Ensure the servers and tools above are installed
       --
@@ -789,6 +964,7 @@ require('lazy').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
+            p(server_name)
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
