c395f129147e0983d0fce135dc4cdda524b08d9a
Francesc Elies <elies@posteo.net>
2025-09-17 16:24:30 +0200

    comment

--- lua/custom/plugins/vim-abolish.lua
+++ lua/custom/plugins/vim-abolish.lua
@@ -1,7 +1,7 @@
 return {
   { 'tpope/vim-abolish' },
-  { 'tpope/vim-unimpaired', event = 'VeryLazy' },
-  { 'tpope/vim-repeat', event = 'VeryLazy' },
+  -- { 'tpope/vim-unimpaired', event = 'VeryLazy' },
+  -- { 'tpope/vim-repeat', event = 'VeryLazy' },
   -- { "wincent/loupe", event = "VeryLazy" },
   -- https://github.com/kevinhwang91/nvim-hlslens
   -- { 'godlygeek/tabular', cmd = 'Tabularize' },
