88e08734521fdd0945f3662a96f8f0e5907ee19e
Francesc elies <elies@posteo.net>
2025-09-20 09:28:28 +0200

    aerial

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -1,7 +1,15 @@
 return {
   'stevearc/aerial.nvim',
-  keys = { '<leader>o', '<cmd>AerialOpen<cr>', desc = 'code [o]utline' },
-  opts = {},
+  keys = {
+    { '<leader>a', '<cmd>AerialToggle!<CR>', 'aerial' },
+  },
+  opts = {
+    on_attach = function(bufnr)
+      -- Jump forwards/backwards with '{' and '}'
+      vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+      vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+    end,
+  },
   -- Optional dependencies
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
