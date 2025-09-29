cb6b10823dd384fbdc216486c7d8689d6ae141dc
Francesc Elies <elies@posteo.net>
2025-09-29 09:01:55 +0200

    remove treesitter objects

--- init.lua
+++ init.lua
@@ -1242,71 +1242,10 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       -- NOTE: additional parser
-      'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    config = function()
-      require('nvim-treesitter-textobjects').setup {
-        select = {
-          lookahead = true, -- Automatically jump forward to textobj
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V', -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
-          },
-        },
-        move = {
-          set_jumps = true,
-        },
-        include_surrounding_whitespace = false,
-      }
-
-    -- stylua: ignore start
-      local TOS = require 'nvim-treesitter-textobjects.select'
-      vim.keymap.set({ 'x', 'o' }, 'af', function() TOS.select_textobject('@function.outer', 'textobjects') end)
-      vim.keymap.set({ 'x', 'o' }, 'if', function() TOS.select_textobject('@function.inner', 'textobjects') end)
-      vim.keymap.set({ 'x', 'o' }, 'ac', function() TOS.select_textobject('@class.outer', 'textobjects') end)
-      vim.keymap.set({ 'x', 'o' }, 'ic', function() TOS.select_textobject('@class.inner', 'textobjects') end)
-      vim.keymap.set({ 'x', 'o' }, 'as', function() TOS.select_textobject('@local.scope', 'locals') end)
-
-      local TOW = require 'nvim-treesitter-textobjects.swap'
-      vim.keymap.set('n', 'sn', function() TOW.swap_next '@parameter.inner' end) -- [s]wap with [n]ext parameter
-      vim.keymap.set('n', 'sp', function() TOW.swap_next '@parameter.outer' end) -- [s]wap with [p]revious parameter
-
-      local TOM = require 'nvim-treesitter-textobjects.move'
-      vim.keymap.set({ 'n', 'x', 'o' }, ']m', function() TOM.goto_next_start('@function.outer', 'textobjects') end)
-      vim.keymap.set({ 'n', 'x', 'o' }, ']]', function() TOM.goto_next_start('@class.outer', 'textobjects') end)
-      -- You can also pass a list to group multiple queries.
-      vim.keymap.set({ 'n', 'x', 'o' }, ']o', function() move.goto_next_start({ '@loop.inner', '@loop.outer' }, 'textobjects') end)
-      -- You can also use captures from other query groups like `locals.scm` or `folds.scm`
-      vim.keymap.set({ 'n', 'x', 'o' }, ']s', function() TOM.goto_next_start('@local.scope', 'locals') end)
-      vim.keymap.set({ 'n', 'x', 'o' }, ']z', function() TOM.goto_next_start('@fold', 'folds') end)
-
-      vim.keymap.set({ 'n', 'x', 'o' }, ']M', function() TOM.goto_next_end('@function.outer', 'textobjects') end)
-      vim.keymap.set({ 'n', 'x', 'o' }, '][', function() TOM.goto_next_end('@class.outer', 'textobjects') end)
-
-      vim.keymap.set({ 'n', 'x', 'o' }, '[m', function() TOM.goto_previous_start('@function.outer', 'textobjects') end)
-      vim.keymap.set({ 'n', 'x', 'o' }, '[[', function() TOM.goto_previous_start('@class.outer', 'textobjects') end)
-
-      vim.keymap.set({ 'n', 'x', 'o' }, '[M', function() TOM.goto_previous_end('@function.outer', 'textobjects') end)
-      vim.keymap.set({ 'n', 'x', 'o' }, '[]', function() TOM.goto_previous_end('@class.outer', 'textobjects') end)
-      -- stylua: ignore end
-
-      local TOR = require 'nvim-treesitter-textobjects.repeatable_move'
-
-      -- Repeat movement with ; and ,
-      -- vim way: ; goes to the direction you were moving.
-      vim.keymap.set({ 'n', 'x', 'o' }, ';', TOR.repeat_last_move)
-      vim.keymap.set({ 'n', 'x', 'o' }, ',', TOR.repeat_last_move_opposite)
-
-      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      -- vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f_expr, { expr = true })
-      -- vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F_expr, { expr = true })
-      -- vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t_expr, { expr = true })
-      -- vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T_expr, { expr = true })
-    end,
     opts = {
       -- 'bash', 'c_sharp', 'cpp', 'go', 'html', 'mermaid', 'typescript', 'query', 'vim', 'sql', 'zig',
       ensure_installed = { 'c', 'diff', 'lua', 'markdown', 'markdown_inline', 'python', 'rust', 'nu', 'vim', 'vimdoc', 'yaml' },
