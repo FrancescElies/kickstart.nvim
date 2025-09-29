d12b9da19d312b9209260bed908ca1a89e3b7158
Francesc Elies <elies@posteo.net>
2025-09-29 10:10:13 +0200

    firenvim

---
+++ lua/custom/plugins/firenvim.lua
@@ -0,0 +1,6 @@
+return {
+  'glacambre/firenvim',
+  -- https://github.com/glacambre/firenvim/blob/master/TROUBLESHOOTING.md
+  -- :call firenvim#install(1)
+  build = ':call firenvim#install(0)',
+}
