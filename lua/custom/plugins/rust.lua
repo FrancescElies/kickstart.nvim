d3d41f644c084d26005b6699e422a88175660098
Francesc Elies <elies@posteo.net>
2025-09-29 08:28:27 +0200

    rust

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -40,60 +40,60 @@
 -- end
 
 return {
-  --   --   disable rust-analyzer in lsp-init.lua if this plugin enabled
-  --   'mrcjkb/rustaceanvim',
-  --   version = '^6', -- Recommended
-  --   lazy = false, -- This plugin is already lazy
-  --
-  --   config = function()
-  --     -- You only need to specify the keys that you want to be changed, because defaults are applied for keys that are not provided.
-  --     vim.g.rustaceanvim = {
-  --       -- Plugin configuration
-  --       -- tools = {},
-  --       -- LSP configuration
-  --       server = {
-  --         -- on_attach = function(client, bufnr)
-  --         --   -- you can also put keymaps in here
-  --         -- end,
-  --         default_settings = {
-  --           -- https://rust-analyzer.github.io/manual.html
-  --           -- see `lsp-init.lua` too
-  --           ['rust-analyzer'] = {
-  --             diagnostics = { experimental = { enable = true } },
-  --             checkOnSave = true, -- disables running clippy on save
-  --             -- checkOnSave = { command = 'clippy', enable = true },
-  --           },
-  --         },
-  --       },
-  --       -- DAP configuration
-  --       -- dap = {},
-  --     }
-  --
-  --     vim.api.nvim_create_autocmd('LspAttach', {
-  --       group = vim.api.nvim_create_augroup('my-rustaceanvim-lsp-attach', { clear = true }),
-  --       callback = function(event)
-  --         local rust_keymap = function(keys, func, desc)
-  --           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'rust: ' .. desc })
-  --         end
-  --
-  --         if vim.bo.filetype == 'rust' then
-  --           rust_keymap('<localleader>a', ':RustLsp codeAction<cr>', 'code [a]ction')
-  --           rust_keymap('<localleader>e', ':RustLsp explainError', '[e]xplain error')
-  --           rust_keymap('<localleader>E', ':RustLsp expandMacro<cr>', '[e]xpand macros')
-  --           rust_keymap('<localleader>f', ':RustLsp flyCheck run', '[f]ly check')
-  --           rust_keymap('<localleader>h', ':RustLsp hover ', '[h]over actions|range')
-  --           rust_keymap('<localleader>j', ':RustLsp joinLines<cr>', 'join lines')
-  --           rust_keymap('<localleader>m', ':RustLsp moveItem ', '[m]ove up|down')
-  --           rust_keymap('<localleader>o', ':RustLsp openCargo<cr>', '[o]pen cargo')
-  --           rust_keymap('<localleader>p', ':RustLsp parentModule<cr>', '[p]arent module')
-  --           rust_keymap('<localleader>t', ':RustLsp! testables<cr>', 'run previous [t]ests')
-  --           rust_keymap('<localleader>T', ':RustLsp testables<cr>', 'run [t]ests')
-  --           rust_keymap('<localleader>w', ':RustLsp workspaceSymbol allSymbols ', '[w]orkspace symbol')
-  --           -- :RustLsp crateGraph {backend {output}}
-  --           -- :RustLsp syntaxTree
-  --           -- :Rustc unpretty {hir|mir|...}
-  --         end
-  --       end,
-  --     })
-  --   end,
+  --   disable rust-analyzer in lsp-init.lua if this plugin enabled
+  'mrcjkb/rustaceanvim',
+  version = '^6', -- Recommended
+  lazy = false, -- This plugin is already lazy
+
+  config = function()
+    -- You only need to specify the keys that you want to be changed, because defaults are applied for keys that are not provided.
+    vim.g.rustaceanvim = {
+      -- Plugin configuration
+      -- tools = {},
+      -- LSP configuration
+      server = {
+        -- on_attach = function(client, bufnr)
+        --   -- you can also put keymaps in here
+        -- end,
+        default_settings = {
+          -- https://rust-analyzer.github.io/manual.html
+          -- see `lsp-init.lua` too
+          ['rust-analyzer'] = {
+            diagnostics = { experimental = { enable = true } },
+            -- checkOnSave = true, -- disables running clippy on save
+            checkOnSave = { command = 'clippy', enable = true },
+          },
+        },
+      },
+      -- DAP configuration
+      -- dap = {},
+    }
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('my-rustaceanvim-lsp-attach', { clear = true }),
+      callback = function(event)
+        local rust_keymap = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'rust: ' .. desc })
+        end
+
+        if vim.bo.filetype == 'rust' then
+          rust_keymap('<localleader>a', ':RustLsp codeAction<cr>', 'code [a]ction')
+          rust_keymap('<localleader>e', ':RustLsp explainError', '[e]xplain error')
+          rust_keymap('<localleader>E', ':RustLsp expandMacro<cr>', '[e]xpand macros')
+          rust_keymap('<localleader>f', ':RustLsp flyCheck run', '[f]ly check')
+          rust_keymap('<localleader>h', ':RustLsp hover ', '[h]over actions|range')
+          rust_keymap('<localleader>j', ':RustLsp joinLines<cr>', 'join lines')
+          rust_keymap('<localleader>m', ':RustLsp moveItem ', '[m]ove up|down')
+          rust_keymap('<localleader>o', ':RustLsp openCargo<cr>', '[o]pen cargo')
+          rust_keymap('<localleader>p', ':RustLsp parentModule<cr>', '[p]arent module')
+          rust_keymap('<localleader>t', ':RustLsp! testables<cr>', 'run previous [t]ests')
+          rust_keymap('<localleader>T', ':RustLsp testables<cr>', 'run [t]ests')
+          rust_keymap('<localleader>w', ':RustLsp workspaceSymbol allSymbols ', '[w]orkspace symbol')
+          -- :RustLsp crateGraph {backend {output}}
+          -- :RustLsp syntaxTree
+          -- :Rustc unpretty {hir|mir|...}
+        end
+      end,
+    })
+  end,
 }
