4c7bdbc82e81d12c1b35b704a153a48e9e9f6a6c
Francesc Elies <elies@posteo.net>
2025-10-09 09:33:22 +0200

    notes

--- lua/custom/plugins/notes.lua
+++ lua/custom/plugins/notes.lua
@@ -5,15 +5,15 @@ local function get_todos(dir, states)
   vim.fn.feedkeys('^ *([*]+|[-]+) +[(]' .. states .. '[)]')
 end
 
-vim.keymap.set('n', '<leader>snt', function()
+vim.keymap.set('n', '<leader>nt', function()
   get_todos('~/src/_notes ', '[^x_]')
 end, { desc = '[s]earch [n]otes [t]odos' })
 
-vim.keymap.set('n', '<leader>sng', function()
+vim.keymap.set('n', '<leader>ng', function()
   tele_builtin.live_grep { cwd = '~/src/_notes/' }
 end, { desc = '[s]earch [n]otes by [g]rep' })
 
-vim.keymap.set('n', '<leader>snf', function()
+vim.keymap.set('n', '<leader>nf', function()
   tele_builtin.find_files { cwd = '~/src/_notes/' }
 end, { desc = '[s]earch [n]otes [f]iles' })
 
