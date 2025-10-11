f1b71393ed159b7084adb2d3fa5f1814d8c1fbf6
Francesc elies <elies@posteo.net>
2025-10-11 21:00:23 +0200

    term

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,8 +1,5 @@
 --https://github.com/akinsho/toggleterm.nvim
 
--- <C-r> doesn't work in terminal mode, it will perform `reverse search`
-vim.keymap.set('t', '<A-r>', "'<C-\\><C-N>\"'.nr2char(getchar()).'pi'", { desc = '<C-r> fellow' })
-
 -- Terminal Mappings
 vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Enter Normal Mode' })
 vim.keymap.set('t', '<C-w><C-h>', '<cmd>wincmd h<cr>', { desc = 'go left window' })
@@ -146,6 +143,9 @@ vim.keymap.set({ 'n', 't' }, '\u{f8ff}', '<cmd>SmallFloatTerm<cr>', { desc = 'fl
 vim.keymap.set({ 'n', 't' }, ',f', '<cmd>SmallFloatTerm<cr>', { desc = 'f[l]oat term' }) -- sf taken by mini.surround
 vim.keymap.set('n', ',l', small_term_send_line, { desc = 'send [l]ine to small term' })
 vim.keymap.set('n', ',s', small_term, { desc = '[s]mall term' })
+vim.keymap.set('t', ',d', '<cmd>bd!<cr>', { desc = 'delete term' })
+-- <C-r> doesn't work in terminal mode, it will perform `reverse search`
+-- vim.keymap.set('t', ',r', "'<C-\\><C-N>\"'.nr2char(getchar()).'pi'", { desc = '<C-r> fellow' })
 
 vim.keymap.set({ 'n', 't' }, ',c', toggle_tgpt, { desc = '[c]hatgpt' })
 
