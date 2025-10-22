42fff4164acdeace7c6cc167a12a2f72be352aca
Francesc Elies <elies@posteo.net>
2025-10-22 13:46:10 +0200

    bindings meta

--- lua/custom/plugins/grapple.lua
+++ lua/custom/plugins/grapple.lua
@@ -8,7 +8,7 @@ return {
   keys = {
     { '<M-m>', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
     { 'M', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle tag' },
-    { 'H', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
-    { 'L', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
+    { '<M-h>', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
+    { '<M-l>', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
   },
 }
