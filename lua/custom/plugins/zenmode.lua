e650614f391919f17b8cec08799ba1373caaaa8c
Francesc elies <elies@posteo.net>
2025-10-25 16:07:22 +0200

    smaller zen

--- lua/custom/plugins/zenmode.lua
+++ lua/custom/plugins/zenmode.lua
@@ -1,5 +1,5 @@
 local function zen_mode()
-  require('zen-mode').setup { window = { width = 130, options = {} } }
+  require('zen-mode').setup { window = { width = 120, options = {} } }
   require('zen-mode').toggle()
   vim.wo.wrap = true
   vim.wo.number = true
