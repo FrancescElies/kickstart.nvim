99c1ca777e76e0a7e287dc4acdcc7987516103c8
Francesc elies <elies@posteo.net>
2025-10-26 06:46:28 +0100

    snippets

--- init.lua
+++ init.lua
@@ -1073,6 +1073,26 @@ require('lazy').setup({
           },
         },
         opts = {},
+        config = function()
+          require 'custom.snippets'
+
+          local ls = require 'luasnip'
+          vim.keymap.set({ 'i' }, '<c-s>', function()
+            ls.expand {}
+          end, { silent = true })
+          vim.keymap.set({ 'i', 's' }, '<c-s>;', function()
+            ls.jump(1)
+          end, { silent = true })
+          vim.keymap.set({ 'i', 's' }, '<c-s>,', function()
+            ls.jump(-1)
+          end, { silent = true })
+
+          vim.keymap.set({ 'i', 's' }, '<c-e>', function()
+            if ls.choice_active() then
+              ls.change_choice(1)
+            end
+          end, { silent = true })
+        end,
       },
       'folke/lazydev.nvim',
     },
