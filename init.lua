ebcb7849ff82b72362f8f471e069f92ba0c36470
Francesc Elies <elies@posteo.net>
2025-10-31 10:59:24 +0100

    filenaem first

--- init.lua
+++ init.lua
@@ -408,9 +408,9 @@ require('lazy').setup({
         defaults = {
           layout_strategy = 'vertical',
           path_display = {
-            shorten = { len = 4, exclude = { -2, -1 } },
-            -- filename_first = { reverse_directories = true },
-            truncate = 3,
+            -- shorten = { len = 4, exclude = { -2, -1 } },
+            filename_first = { reverse_directories = true },
+            -- truncate = 3,
           },
           mappings = {
             i = {
