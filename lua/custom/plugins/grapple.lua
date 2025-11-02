8bc4e09dcdf4a0486fb7c933526dc9f04f36d52a
Francesc elies <elies@posteo.net>
2025-11-02 19:49:41 +0100

    remove comment

--- lua/custom/plugins/grapple.lua
+++ lua/custom/plugins/grapple.lua
@@ -16,31 +16,4 @@ return {
       { 'L', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
     },
   },
-  -- {
-  --   'otavioschwanck/arrow.nvim',
-  --   dependencies = {
-  --     { 'nvim-tree/nvim-web-devicons' },
-  --     -- or if using `mini.icons`
-  --     -- { "echasnovski/mini.icons" },
-  --   },
-  --   opts = {
-  --     show_icons = true,
-  --     leader_key = 'M',
-  --     buffer_leader_key = '\\', -- Per Buffer Mappings
-  --   },
-  --   keys = {
-  --     {
-  --       'H',
-  --       function()
-  --         require('arrow.persist').previous()
-  --       end,
-  --     },
-  --     {
-  --       'L',
-  --       function()
-  --         require('arrow.persist').next()
-  --       end,
-  --     },
-  --   },
-  -- },
 }
