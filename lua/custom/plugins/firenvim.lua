1fa5b070e196b2557042ca64a46c26862be6d457
Francesc Elies <elies@posteo.net>
2025-11-04 07:25:24 +0100

    not using it

--- lua/custom/plugins/firenvim.lua
+++ lua/custom/plugins/firenvim.lua
@@ -1,6 +1,6 @@
 return {
-  'glacambre/firenvim',
-  -- https://github.com/glacambre/firenvim/blob/master/TROUBLESHOOTING.md
-  -- :call firenvim#install(1)
-  build = ':call firenvim#install(0)',
+  -- 'glacambre/firenvim',
+  -- -- https://github.com/glacambre/firenvim/blob/master/TROUBLESHOOTING.md
+  -- -- :call firenvim#install(1)
+  -- build = ':call firenvim#install(0)',
 }
