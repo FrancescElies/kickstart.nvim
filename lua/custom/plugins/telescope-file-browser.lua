2ee93e0b53ab1c1f63aa67f2e573cafa19627094
Francesc elies <elies@posteo.net>
2025-11-07 06:51:52 +0100

    remove plugin

--- lua/custom/plugins/telescope-file-browser.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-telescope/telescope-file-browser.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-  keys = {
-    { '<space>fB', ':Telescope file_browser<CR>' },
-    { '<space>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>' },
-  },
-}
