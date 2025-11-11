cbb689372db493f88d557f365aff1467031df19c
Francesc Elies <elies@posteo.net>
2025-11-11 09:36:42 +0100

    treesitter-context

---
+++ lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      enable = true,
+      max_lines = '20%', -- How many lines the window should span. Values <= 0 mean no limit.
+      multiwindow = true,
+    },
+  },
+}
