facc8476b219a87f719a1ba3d3974498c033c323
Francesc Elies <elies@posteo.net>
2025-11-12 13:57:17 +0100

    tsc

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -6,5 +6,8 @@ return {
       max_lines = '20%', -- How many lines the window should span. Values <= 0 mean no limit.
       multiwindow = true,
     },
+    keys = {
+      { '<leader>vc', ':TSContext toggle<cr>', desc = '[v]im treesitter-[c]ontext toggle' },
+    },
   },
 }
