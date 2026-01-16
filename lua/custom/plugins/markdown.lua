51378f696f59e0463f181cba2660a00148760a1d
Francesc Elies <elies@posteo.net>
2026-01-16 09:43:00 +0100

    open azure devops link

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,3 +1,19 @@
+local azure_org = vim.env.ADO_ORGANIZATION
+local azure_project = vim.env.ADO_PROJECT
+
+local function open_azure_devops_link_under_cursor()
+  local number = vim.fn.expand '<cword>'
+  if tonumber(number) then
+    local url = string.format('https://dev.azure.com/%s/%s/_workitems/edit/%s', azure_org, azure_project, number)
+    vim.ui.open(url)
+  else
+    print 'No valid number under cursor'
+  end
+end
+
+vim.api.nvim_create_user_command('AzureDevOpsOpen', open_azure_devops_link_under_cursor, {})
+vim.keymap.set('n', 'gX', open_azure_devops_link_under_cursor, { desc = 'Open Azure DevOps link' })
+
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
