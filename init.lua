5a37f2652573cc87f9f211e6feebd9d8e1c4ec10
Francesc Elies <elies@posteo.net>
2026-01-30 08:42:04 +0100

    no harper

--- init.lua
+++ init.lua
@@ -725,17 +725,17 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        harper_ls = {
-          settings = {
-            ['harper-ls'] = {
-              userDictPath = vim.fn.expand '~/src/kickstart.nvim/dict.txt',
-              linters = {
-                SentenceCapitalization = false,
-                SpellCheck = false,
-              },
-            },
-          },
-        },
+        -- harper_ls = { -- typos
+        --   settings = {
+        --     ['harper-ls'] = {
+        --       userDictPath = vim.fn.expand '~/src/kickstart.nvim/dict.txt',
+        --       linters = {
+        --         SentenceCapitalization = false,
+        --         SpellCheck = false,
+        --       },
+        --     },
+        --   },
+        -- },
         clangd = {
           cmd = {
             'clangd',
