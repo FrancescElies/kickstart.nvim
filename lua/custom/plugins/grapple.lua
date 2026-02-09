421b22b78b655cc095e7ba3f177b2686d80902c5
Francesc Elies <elies@posteo.net>
2026-02-09 06:42:41 +0100

    go back to just vim marks

--- lua/custom/plugins/grapple.lua
+++ lua/custom/plugins/grapple.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    'cbochs/grapple.nvim',
-    opts = {
-      scope = 'git', -- also try out "git_branch"
-    },
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = {
-      { 'nvim-tree/nvim-web-devicons', lazy = true },
-    },
-    cmd = 'Grapple',
-    keys = {
-      { 'M', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
-      { 'm.', '<cmd>Grapple tag<cr>', desc = 'Grapple add tag' },
-      { 'H', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
-      { 'L', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
-    },
-  },
+  -- {
+  --   'cbochs/grapple.nvim',
+  --   opts = {
+  --     scope = 'git', -- also try out "git_branch"
+  --   },
+  --   event = { 'BufReadPost', 'BufNewFile' },
+  --   dependencies = {
+  --     { 'nvim-tree/nvim-web-devicons', lazy = true },
+  --   },
+  --   cmd = 'Grapple',
+  --   keys = {
+  --     { 'M', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
+  --     { 'm.', '<cmd>Grapple tag<cr>', desc = 'Grapple add tag' },
+  --     { 'H', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
+  --     { 'L', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
+  --   },
+  -- },
 }
