8538e7abd0371a611b6aeba5f629f2fb4a73433e
Francesc Elies <elies@posteo.net>
2026-02-09 06:40:01 +0100

    TSContext

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-context',
     opts = {
-      enable = true,
+      enable = false,
       max_lines = '20%', -- How many lines the window should span. Values <= 0 mean no limit.
       separator = '-',
     },
