31e8df94a80a6b258706191200c8a040ac80be41
Francesc Elies <elies@posteo.net>
2026-02-11 10:16:59 +0100

    fix

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -106,10 +106,10 @@ return {
     cmd = { 'G', 'Git', 'Gdiffsplit', 'Gread', 'Gwrite', 'Ggrep', 'GMove', 'GDelete', 'GBrowse', 'GRemove', 'GRename', 'Glgrep', 'Gedit' },
     ft = { 'fugitive' },
     keys = {
-      { '<leader>gb', ':Git blame<cr>', desc = 'Git blame' },
-      { '<leader>gc', ':Git commit', desc = 'Git commit' },
-      { '<leader>gp', ':Git push<cr>', desc = 'Git commit' },
-      { '<leader>g.', ':Git status<cr>', desc = 'Git status' },
+      { '<leader>gb', '<cmd>Git blame<cr>', desc = 'Git blame' },
+      { '<leader>gc', '<cmd>Git commit<cr>', desc = 'Git commit' },
+      { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git commit' },
+      { '<leader>g.', '<cmd>Git status<cr>', desc = 'Git status' },
 
       { '<leader>fu', vim.cmd.Git, desc = 'fugitive' },
     },
