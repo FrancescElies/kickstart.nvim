be51add38a04d24fa24914a8b315c60c67354f2a
Francesc Elies <elies@posteo.net>
2026-02-19 06:48:11 +0100

    mail

--- after/ftplugin/mail.lua
+++ after/ftplugin/mail.lua
@@ -7,11 +7,11 @@ vim.o.textwidth = 72
 vim.o.wrap = true
 
 -- wrap while typing, respect comments (">" quoting), auto-format paragraphs
-vim.opt_local.formatoptions:append('t') -- auto-wrap text
-vim.opt_local.formatoptions:append('c') -- auto-wrap comments (quoted emails)
-vim.opt_local.formatoptions:append('r') -- insert comment leader on enter
-vim.opt_local.formatoptions:append('o') -- continue comments when using 'o' or 'o'
-vim.opt_local.formatoptions:append('l') -- do not break long lines automatically when editing existing text
+vim.opt_local.formatoptions:append 't' -- auto-wrap text
+vim.opt_local.formatoptions:append 'c' -- auto-wrap comments (quoted emails)
+vim.opt_local.formatoptions:append 'r' -- insert comment leader on enter
+vim.opt_local.formatoptions:append 'o' -- continue comments when using 'o' or 'o'
+vim.opt_local.formatoptions:append 'l' -- do not break long lines automatically when editing existing text
 
 -- recognize email quoting
 vim.o.comments = 'b:>'
@@ -24,3 +24,6 @@ vim.o.spelllang = 'en'
 vim.o.list = true
 vim.o.listchars = 'trail:·'
 
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
