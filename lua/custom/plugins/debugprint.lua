4c4f8d00515d33a9187219200ca8e3959ac5a5ef
Francesc Elies <elies@posteo.net>
2026-03-04 11:42:58 +0100

    debugprint

--- lua/custom/plugins/debugprint.lua
+++ lua/custom/plugins/debugprint.lua
@@ -1,6 +1,33 @@
 return {
   'andrewferrier/debugprint.nvim',
-  opts = {}, -- don't delete otherwise keymaps not loaded
+  opts = {
+    keymaps = {
+      normal = {
+        plain_below = 'ydp',
+        plain_above = 'ydP',
+        variable_below = 'ydv',
+        variable_above = 'ydV',
+        variable_below_alwaysprompt = '',
+        variable_above_alwaysprompt = '',
+        surround_plain = 'ydsp',
+        surround_variable = 'ydsv',
+        surround_variable_alwaysprompt = '',
+        textobj_below = 'ydo',
+        textobj_above = 'ydO',
+        textobj_surround = 'ydso',
+        toggle_comment_debug_prints = 'ydt',
+        delete_debug_prints = 'ydd',
+      },
+      insert = {
+        -- plain = '<C-G>p',
+        -- variable = '<C-G>v',
+      },
+      visual = {
+        variable_below = 'ydv',
+        variable_above = 'ydV',
+      },
+    },
+  },
   dependencies = {
     'echasnovski/mini.nvim', -- Optional: Needed for line highlighting
     'nvim-telescope/telescope.nvim', -- Optional: If you want to use the :SearchDebugPrints command with telescope.nvim
