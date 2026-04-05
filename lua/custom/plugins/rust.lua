3ab2b10f7649e3a9c8a21c2f4218396a4877de04
Francesc Elies <elies@posteo.net>
2026-04-05 13:02:21 +0200

    rust

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -101,31 +101,27 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('my-rustaceanvim-lsp-attach', { clear = true }),
         callback = function(event)
-          local rust_keymap = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'rust: ' .. desc })
+          local keymap = function(mode, keys, func, desc)
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'rust: ' .. desc })
           end
 
           if vim.bo.filetype == 'rust' then
-            rust_keymap('<leader>cC', function()
+            keymap('n','<leader>cC', function()
               vim.cmd.RustAnalyzer { 'config', "{ checkOnSave = { command = 'clippy', enable = true } }" }
             end, '[c]lippy on save')
-            rust_keymap('<leader>cc', function()
+            keymap('n','<leader>cc', function()
               vim.cmd.RustAnalyzer { 'config', '{ checkOnSave = true }' }
             end, '[c]heck on save')
-            rust_keymap('<leader>ca', function()
-              vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
-              -- or vim.lsp.buf.codeAction() if you don't want grouping.
-            end, 'code [a]ction')
-            rust_keymap('<leader>ce', ':RustLsp explainError', '[e]xplain error')
-            rust_keymap('<leader>cE', ':RustLsp expandMacro<cr>', '[e]xpand macros')
-            rust_keymap('K', '<cmd>RustLsp hover actions<cr>', '[h]over actions')
-            rust_keymap('J', '<cmd>RustLsp joinLines<cr>', 'join lines')
-            rust_keymap('<leader>cm', ':RustLsp moveItem ', '[m]ove up|down')
-            rust_keymap('<leader>co', ':RustLsp openCargo<cr>', '[o]pen cargo')
-            rust_keymap('<leader>cp', ':RustLsp parentModule<cr>', '[p]arent module')
-            rust_keymap('<leader>c.', ':RustLsp! testables<cr>', 'run previous [t]ests')
-            rust_keymap('<leader>ct', ':RustLsp testables<cr>', 'run [t]ests')
-            -- rust_keymap('<leader>cw', ':RustLsp workspaceSymbol allSymbols ', '[w]orkspace symbol')
+            keymap('n','<leader>ce', ':RustLsp explainError', '[e]xplain error')
+            keymap('n','<leader>cE', ':RustLsp expandMacro<cr>', '[e]xpand macros')
+            keymap('n','K', '<cmd>RustLsp hover actions<cr>', '[h]over actions')
+            keymap('n','J', '<cmd>RustLsp joinLines<cr>', 'join lines')
+            keymap('n','<leader>cm', ':RustLsp moveItem ', '[m]ove up|down')
+            keymap('n','<leader>co', ':RustLsp openCargo<cr>', '[o]pen cargo')
+            keymap('n','<leader>cp', ':RustLsp parentModule<cr>', '[p]arent module')
+            keymap('n','<leader>c.', ':RustLsp! testables<cr>', 'run previous [t]ests')
+            keymap('n','<leader>ct', ':RustLsp testables<cr>', 'run [t]ests')
+            -- rust_keymap('n','<leader>cw', ':RustLsp workspaceSymbol allSymbols ', '[w]orkspace symbol')
             -- :RustLsp crateGraph {backend {output}}
             -- :RustLsp syntaxTree
             -- :Rustc unpretty {hir|mir|...}
