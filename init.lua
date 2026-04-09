cb573b31b3cd3ce9a39d14c29c4cc26c5345cb59
Francesc Elies <elies@posteo.net>
2026-04-09 12:36:14 +0200

    toggle inlay hints

--- init.lua
+++ init.lua
@@ -733,9 +733,9 @@ require('lazy').setup {
           --
           -- This may be unwanted, since they displace some of your code
           if client and client:supports_method('textDocument/inlayHint', event.buf) then
-            map('<leader>th',
+            map('<leader>vh',
               function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end,
-              '[T]oggle Inlay [H]ints')
+              '[v]im Toggle Inlay [H]ints')
           end
         end,
       })
