7e3c82fec5d3ff205d16134a8d8a72cb3ddc27d5
Francesc Elies <elies@posteo.net>
2026-05-06 18:44:27 +0200

    rust calls string based based calls

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -6,6 +6,69 @@
 --   vim.cmd('copen')
 -- end, {})
 
+
+vim.api.nvim_create_user_command("RustFnsNamedAndStringBasedCalls", function(opts)
+  local target = opts.args
+  if target == "" then
+    print("Usage: :RustFnsNamed function_name")
+    return
+  end
+
+  local qf = {}
+  local files = vim.fn.systemlist("rg --files -g '*.rs'")
+
+  local query = vim.treesitter.query.parse("rust", ([[
+    (function_item
+      name: (identifier) @name) @function
+
+    ; ; (macro_definition
+    ; ;   name: (identifier) @name) @macro
+    ;
+    (call_expression
+    function: (identifier)
+    arguments: (arguments (string_literal (string_content) @name))
+                (#eq? @name "%s")) @expression
+  ]]):format(target))
+
+  for _, file in ipairs(files) do
+    local bufnr = vim.fn.bufadd(file)
+    vim.fn.bufload(bufnr)
+
+    local ok, parser = pcall(vim.treesitter.get_parser, bufnr, "rust")
+    if ok and parser then
+      local tree = parser:parse()[1]
+      local root = tree:root()
+
+      for _, match in query:iter_matches(root, bufnr, 0, -1) do
+        local name_node = match[1]
+        local item_node = match[2] or match[3]
+
+        if type(name_node) == "table" then name_node = name_node[1] end
+        if type(item_node) == "table" then item_node = item_node[1] end
+
+        local name = vim.treesitter.get_node_text(name_node, bufnr)
+
+        if name == target then
+          local row, col = item_node:start()
+          local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false)[1]
+
+          table.insert(qf, {
+            filename = file,
+            lnum = row + 1,
+            col = col + 1,
+            text = line,
+          })
+        end
+      end
+    end
+  end
+
+  vim.fn.setqflist(qf, "r")
+  vim.cmd("copen")
+end, {
+  nargs = 1,
+})
+
 return {
   {
     'Canop/nvim-bacon',
@@ -86,14 +149,14 @@ return {
               lens = {
                 enable = true,
                 references = {
-                  adt = { enable = true }, -- structs/enums/unions
+                  adt = { enable = true },    -- structs/enums/unions
                   enumVariant = { enable = true },
                   method = { enable = true }, -- method-level specifically
                   trait = { enable = true },
                 },
                 updateTest = { enable = true },
                 implementations = { enable = true },
-                run = { enable = true }, --  ▶ Run button
+                run = { enable = true },   --  ▶ Run button
                 debug = { enable = true }, -- ▶ Debug button
               },
             },
@@ -111,23 +174,23 @@ return {
           end
 
           if vim.bo.filetype == 'rust' then
-            keymap('n','<leader>cC', function()
+            keymap('n', '<leader>cC', function()
               vim.cmd.RustAnalyzer { 'config', "{ checkOnSave = { command = 'clippy', enable = true } }" }
             end, '[c]lippy on save')
-            keymap('n','<leader>cc', function()
+            keymap('n', '<leader>cc', function()
               vim.cmd.RustAnalyzer { 'config', '{ checkOnSave = true }' }
             end, '[c]heck on save')
-            keymap('n','<leader>ce', ':RustLsp explainError', '[e]xplain error')
-            keymap('n','<leader>cE', ':RustLsp expandMacro<cr>', '[e]xpand macros')
-            keymap('n','K', '<cmd>RustLsp hover actions<cr>', '[h]over actions')
-            keymap('n','J', '<cmd>RustLsp joinLines<cr>', 'join lines')
-            keymap('n','<leader>cm', ':RustLsp moveItem ', '[m]ove up|down')
-            keymap('n','<leader>co', ':RustLsp openCargo<cr>', '[o]pen cargo')
-            keymap('n','<leader>cp', ':RustLsp parentModule<cr>', '[p]arent module')
+            keymap('n', '<leader>ce', ':RustLsp explainError', '[e]xplain error')
+            keymap('n', '<leader>cE', ':RustLsp expandMacro<cr>', '[e]xpand macros')
+            keymap('n', 'K', '<cmd>RustLsp hover actions<cr>', '[h]over actions')
+            keymap('n', 'J', '<cmd>RustLsp joinLines<cr>', 'join lines')
+            keymap('n', '<leader>cm', ':RustLsp moveItem ', '[m]ove up|down')
+            keymap('n', '<leader>co', ':RustLsp openCargo<cr>', '[o]pen cargo')
+            keymap('n', '<leader>cp', ':RustLsp parentModule<cr>', '[p]arent module')
             -- keymap('n','<leader>c.', ':RustLsp! testables<cr>', 'run previous [t]ests')
             -- keymap('n','<leader>ct', ':RustLsp testables<cr>', 'run [t]ests')
             -- rust_keymap('n','<leader>cw', ':RustLsp workspaceSymbol allSymbols ', '[w]orkspace symbol')
-            keymap('n','<leader>cg', ':RustLsp crateGraph', 'crate [g]raph')
+            keymap('n', '<leader>cg', ':RustLsp crateGraph', 'crate [g]raph')
             -- :RustLsp syntaxTree
             -- :Rustc unpretty {hir|mir|...}
           end
