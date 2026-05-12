79f3909287db9c8f1121238057bfe26c20142bce
Francesc Elies <elies@posteo.net>
2026-05-12 22:52:21 +0200

    keymap

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -29,7 +29,7 @@ vim.o.spell = true
 
 -- for faster spelling corrections
 vim.keymap.set('n', 'cp', ':silent! normal! [s1z=<cr>', { desc = '[c]orrect spell [p]revious' })
-vim.keymap.set('n', 'cc', ':silent! norm! 1z=<cr>', { desc = '[c]orrect spell [c]urrent' })
+vim.keymap.set('n', 'c.', ':silent! norm! 1z=<cr>', { desc = '[c]orrect spell [.]current' })
 
 -- File
 local is_windows = vim.uv.os_uname().sysname == "Windows_NT"
