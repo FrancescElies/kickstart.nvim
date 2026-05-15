5fc4ab030a3fffe3fbae9c2f04be176c661eda5f
Francesc Elies <elies@posteo.net>
2026-05-15 19:50:18 +0200

    egui simple plot

--- lua/custom/snippets/rust.lua
+++ lua/custom/snippets/rust.lua
@@ -21,3 +21,46 @@ ls.add_snippets('rust', {
     t { 'eprintln!("BACKTRACE: {}:{}\\n{}", file!(), line!(), std::backtrace::Backtrace::capture());' },
   }),
 })
+
+
+
+-- Simple plot
+-- use eframe::egui;
+-- use egui_plot::{Line, Plot, PlotPoints};
+--
+-- struct App {
+--     data: Vec<[f64; 2]>,
+--     t: f64,
+-- }
+--
+-- impl eframe::App for App {
+--
+--     fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+--         // Simulate incoming data
+--         self.data.push([self.t, self.t.sin()]);
+--         self.t += 0.05;
+--
+--         egui::CentralPanel::default().show_inside(ui, |ui| {
+--             Plot::new("streaming_plot")
+--                 .show(ui, |plot_ui| {
+--                     plot_ui.line(Line::new("aline", PlotPoints::from(self.data.clone())));
+--                 });
+--         });
+--
+--         ui.request_repaint(); // keep updating
+--     }
+-- }
+--
+-- fn main() {
+--     let app = App {
+--         data: vec![],
+--         t: 0.0,
+--     };
+--
+--     eframe::run_native(
+--         "Streaming Plot",
+--         eframe::NativeOptions::default(),
+--         Box::new(|_| Ok(Box::new(app))),
+--     )
+--     .unwrap();
+-- }
