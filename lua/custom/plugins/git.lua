27d743812fd28e9ecdac275f661cd319864a478a
Francesc Elies <elies@posteo.net>
2026-05-29 15:28:41 +0200

    savepoint

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -154,6 +154,17 @@ return {
     },
   },
   {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      gitbrowse = {
+        -- your gitbrowse configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    }
+  },
+  {
     url = 'https://codeberg.org/trevorhauter/gitportal.nvim',
     opts = {
       always_include_current_line = true,
