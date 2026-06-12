3ef6d1dbffaa2fc23c1276d0b114dd40393e15fa
Francesc Elies <elies@posteo.net>
2026-06-12 12:19:21 +0200

    quickfix

--- lua/custom/plugins/quickfix.lua
+++ lua/custom/plugins/quickfix.lua
@@ -6,7 +6,9 @@ local fn = require 'custom.fn'
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.keymap.set({ 'n', 'v' }, '<leader>qx', vim.lsp.buf.references, { buffer = true, desc = 'references to quickfix' })
+vim.keymap.set({ 'n', 'v' }, '<leader>qr', vim.lsp.buf.references, { buffer = true, desc = '[q]uickfix [r]references' })
+vim.keymap.set('n', '<leader>qx', vim.cmd [[cclose]], { desc = '[q]uickfix [x]close' })
+vim.keymap.set('n', '<leader>qo', vim.cmd [[copen]], { desc = '[q]uickfix [o]pen' })
 vim.keymap.set('n', '<leader>qb', vim.diagnostic.setloclist, { desc = '[q]uickfix [b]uffer diag.' })
 vim.keymap.set('n', '<leader>qa', vim.diagnostic.setqflist, { desc = '[q]uickfix all [d]iag.' })
 vim.keymap.set('n', '<leader>qw', function() vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN } end, { desc = '[q]uickfix diag. [w]arnings' })
