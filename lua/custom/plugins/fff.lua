7303f3d53cd83c3c87d5aedbe2c08cabc5de27b0
Francesc Elies <elies@posteo.net>
2026-07-06 13:57:51 +0200

    fff

--- lua/custom/plugins/fff.lua
+++ lua/custom/plugins/fff.lua
@@ -12,11 +12,15 @@ vim.api.nvim_create_autocmd('PackChanged', {
 
 vim.g.fff = {
   lazy_sync = true,
-  debug = { enabled = true, show_scores = true },
+  debug = { enabled = false, show_scores = true },
 }
 
 vim.keymap.set('n', 'ff', function() require('fff').find_files() end, { desc = 'FFFind files' })
 vim.keymap.set('n', 'fg', function() require('fff').live_grep() end, { desc = 'LiFFFe grep files' })
 vim.keymap.set('n', 'fw', function() require('fff').live_grep_under_cursor() end, { desc = 'FFFind current word / selection' })
-vim.keymap.set('n', 'fr', function() require('fff').change_indexing_directory(vim.fn.expand '%:p:h') end, { desc = 'FFFind change root' })
+-- vim.keymap.set('n', 'fr', function()
+--   local dir = vim.fn.expand '%:p:h'
+--   require('fff').change_indexing_directory(dir)
+--   vim.notify('fff uses now ' .. dir)
+-- end, { desc = 'FFFind change root' })
 vim.keymap.set('n', 'fs', function() require('fff').scan_files() end, { desc = 'FFFind rescan' })
