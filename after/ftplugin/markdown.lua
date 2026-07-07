712c9d9d5ad695bc9d8de4662ada871a082b5133
Francesc Elies <elies@posteo.net>
2026-07-07 21:37:58 +0200

    md

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,22 @@
-vim.o.textwidth = 80
-vim.o.wrap = false
+vim.opt_local.textwidth = 80
+
+-- Enable spelling and wrap for window
+vim.cmd('setlocal spell wrap')
+
+
+-- Set markdown-specific surrounding in 'mini.surround'
+vim.b.minisurround_config = {
+  custom_surroundings = {
+    -- Markdown link. Common usage:
+    -- `saiwL` + [type/paste link] + <CR> - add link
+    -- `sdL` - delete link
+    -- `srLL` + [type/paste link] + <CR> - replace link
+    L = {
+      input = { '%[().-()%]%(.-%)' },
+      output = function()
+        local link = require('mini.surround').user_input('Link: ')
+        return { left = '[', right = '](' .. link .. ')' }
+      end,
+    },
+  },
+}
