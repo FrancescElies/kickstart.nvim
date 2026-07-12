dd637a7b718061839d6fc696b440f055add93327
Francesc Elies <elies@posteo.net>
2026-07-12 19:37:12 +0200

    lsp

--- lua/custom/lsp-servers.lua
+++ lua/custom/lsp-servers.lua
@@ -128,9 +128,8 @@ local servers = {
   --
   -- Some languages (like typescript) have entire language plugins that can be useful:
   --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`tsserver`) will work just fine
-  ts_ls = {},
+  -- But for many setups, the LSP (`ts_ls`) will work just fine
+  -- ts_ls = {},
   --
   -- powershell_es = {
   --   settings = {
