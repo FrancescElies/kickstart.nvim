edc7979e4cd746dc5ab4114cd3f8f228b78c0ced
Francesc Elies <elies@posteo.net>
2026-08-04 15:41:50 +0200

    show pid

--- init.lua
+++ init.lua
@@ -390,6 +390,7 @@ do
           '%=',
           { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
           { hl = mode_hl, strings = { location } },
+          { hl = mode_hl, strings = { 'pid:' .. pid } },
           { hl = 'MiniStatuslineFilename', strings = { 'pid:' .. pid } },
         }
       end,
