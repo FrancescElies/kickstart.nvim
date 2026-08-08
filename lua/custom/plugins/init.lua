089dd4cdd1a863dad269335e7e7203d0545b864e
Francesc Elies <elies@posteo.net>
2026-08-08 08:58:37 +0200

    bracketed

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -263,6 +263,8 @@ vim.pack.add {
   -- fn.gh 'godlygeek/tabular'
 }
 
+require('mini.bracketed').setup {}
+
 -- Iterate over all Lua files in the plugins directory and load them
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
 for file_name, type in vim.fs.dir(plugins_dir) do
