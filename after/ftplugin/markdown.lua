315efe3e3350b67cd46daaa90200916f089f6b6a
Francesc Elies <elies@posteo.net>
2026-08-09 08:48:19 +0200

    md to html

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,15 +1,15 @@
 vim.keymap.set('n', '<localleader><localleader>', function()
   local file = vim.fn.expand '%:p'
   vim.cmd 'write'
-  local output = vim.fn.fnamemodify(file, ':r') .. '.pdf'
-  vim.fn.jobstart({ 'typst', 'compile', file, output }, {
+  local output = vim.fn.fnamemodify(file, ':r') .. '.html'
+  vim.fn.jobstart({ 'makurust', file }, {
     on_exit = function(_, code)
       if code == 0 then
-        vim.notify('Typst compiled: ' .. output, vim.log.levels.INFO)
+        vim.notify('makurust: ' .. output, vim.log.levels.INFO)
         local fn = require 'custom.fn'
         fn.open_in_system_default_app(output)
       else
-        vim.notify('Typst compile failed (exit ' .. code .. ')', vim.log.levels.ERROR)
+        vim.notify('makurust failed (exit ' .. code .. ')', vim.log.levels.ERROR)
       end
     end,
     stderr_buffered = true,
@@ -17,4 +17,4 @@ vim.keymap.set('n', '<localleader><localleader>', function()
       if data and #data > 1 then vim.notify(table.concat(data, '\n'), vim.log.levels.ERROR) end
     end,
   })
-end, { buffer = 0, desc = 'Compile Typst to PDF' })
+end, { buffer = 0, desc = 'Compile markdown to html' })
