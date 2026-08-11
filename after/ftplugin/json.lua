d765f6a2d35575d6ad5b42bd514f811c44811d0d
Francesc Elies <elies@posteo.net>
2026-08-11 18:53:52 +0200

    json path

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -1,43 +1,45 @@
-local function json_key_path()
-  local bufnr = 0
+local function json_path_at_cursor()
   local node = vim.treesitter.get_node()
-  local root = node:tree():root()
-
   if not node then
-    vim.notify('No Treesitter node', vim.log.levels.ERROR)
+    vim.notify('No treesitter node at cursor (run :TSInstall json)', vim.log.levels.WARN)
     return
   end
 
+  local bufnr = vim.api.nvim_get_current_buf()
   local parts = {}
 
-  local cur = root
+  while node do
+    local parent = node:parent()
+    if not parent then break end
 
-  while cur do
-    local child = cur:child_with_descendant(node)
-    if not child or child:id() == cur:id() then break end
-    local t = child:type()
-    -- Seen types: array pair object string string_content
+    local ptype = parent:type()
 
-    if t == 'pair' then
-      local key_node = node:child(0)
-      if key_node then
-        local key = vim.treesitter.get_node_text(key_node, bufnr)
-        key = key:gsub('^"', ''):gsub('"$', '')
-        table.insert(parts, 1, key)
+    if ptype == 'pair' then
+      local key_field = parent:field('key')[1]
+      local val_field = parent:field('value')[1]
+      if node == key_field or node == val_field then
+        local key_text = vim.treesitter.get_node_text(key_field, bufnr)
+        key_text = key_text:gsub('^"(.*)"$', '%1')
+        table.insert(parts, 1, '.' .. key_text)
       end
-    elseif t == 'object' then
-      print('DEBUGPRINT[232]: json.lua:28: t=' .. vim.inspect(t))
-    else
-      print('DEBUGPRINT[231]: json.lua:30: t= unhandled' .. vim.inspect(t))
+    elseif ptype == 'array' then
+      local idx = 0
+      for i = 0, parent:named_child_count() - 1 do
+        if parent:named_child(i) == node then
+          idx = i
+          break
+        end
+      end
+      table.insert(parts, 1, '[' .. idx .. ']')
     end
 
-    cur = child
+    node = parent
   end
 
-  local path = table.concat(parts, '.')
+  local path = '$' .. table.concat(parts)
   vim.fn.setreg('+', path)
-  vim.notify('Yanked: ' .. path)
+  vim.notify('JSONPath: ' .. path, vim.log.levels.INFO)
+  return path
 end
 
-vim.api.nvim_create_user_command('JsonKeyPath', json_key_path, {})
-vim.keymap.set('n', '<localleader>k', json_key_path, { buffer = 0, desc = '[k]ey path' })
+vim.keymap.set('n', '<localleader>p', json_path_at_cursor, { desc = 'Yank JSONPath under cursor' })
