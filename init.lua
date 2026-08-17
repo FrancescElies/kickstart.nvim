0d4876376c5964b23d8ab58c9e794a3c6c4e4e2f
Francesc Elies <elies@posteo.net>
2026-08-17 21:30:31 +0200

    clean consecutive empty lines

--- init.lua
+++ init.lua
@@ -405,7 +405,8 @@ do
 
   local mini_trailspace = require 'mini.trailspace'
   mini_trailspace.setup()
-  vim.keymap.set('n', '<localleader><space>', function() mini_trailspace.trim() end, { desc = 'clean trailspaces' })
+  vim.keymap.set('n', '<localleader>T', function() mini_trailspace.trim() end, { desc = 'Trailspaces' })
+  vim.keymap.set('n', '<localleader><space>', ':g/^$/,/./-j', { desc = 'clean consecutive empty lines' })
 
   -- - ga - [a]lign
   -- - gA - [a]lign with preview
