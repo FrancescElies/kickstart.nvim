6ee0a28faa622f03ee15652d2657bc16ae35e1ca
Francesc Elies <elies@posteo.net>
2026-08-21 09:15:26 +0200

    ts-context: bindings

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -15,4 +15,5 @@ tscontext.setup {
 }
 
 vim.keymap.set('n', '<leader>to', '<cmd>TSContext toggle<cr>', { desc = 'toggle TS-c[o]ntext' })
-vim.keymap.set('n', 'gco', function() tscontext.go_to_context(vim.v.count1) end, { silent = true, desc = 'goto c[o]ntext' })
+-- [p is one of the few free keys :/, is nicely placed close to [
+vim.keymap.set('n', '[p', function() tscontext.go_to_context(vim.v.count1) end, { silent = true, desc = 'prev context' })
