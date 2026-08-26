8f704f204d0794beb5a4be25ea19039185d2df84
Francesc Elies <elies@posteo.net>
2026-08-26 10:31:24 +0200

    write before run

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,2 +1,5 @@
 local fn = require 'custom.fn'
-vim.keymap.set('n', '<localleader><localleader>', function() fn.run_async { 'npm', 'run', 'build' } end, { buffer = 0, desc = 'npm run build' })
+vim.keymap.set('n', '<localleader><localleader>', function()
+  vim.cmd 'wall'
+  fn.run_async { 'npm', 'run', 'build' }
+end, { buffer = 0, desc = 'npm run build' })
