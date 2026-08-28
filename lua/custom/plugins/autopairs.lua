cac5e11efb0d68e0cbf98f18652cbfe782d610c9
Francesc Elies <elies@posteo.net>
2026-08-28 08:13:30 +0200

    autopairs: disable for fff input

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -2,4 +2,6 @@
 -- https://github.com/windwp/nvim-autopairs
 
 vim.pack.add { 'https://github.com/windwp/nvim-autopairs' }
-require('nvim-autopairs').setup {}
+require('nvim-autopairs').setup {
+  disable_filetype = { 'TelescopePrompt', 'spectre_panel', 'snacks_picker_input', 'fff_input' },
+}
